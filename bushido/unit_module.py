3f27ed4ba218bf755d2c92c9b2ada21d24952378
Noe Javet <javetnoe@students.zhaw.ch>
2024-05-29 18:19:01 +0200

    clean up

--- bushido/unit_module.py
+++ bushido/unit_module.py
@@ -1,5 +1,6 @@
 import collections
 
+import helpers
 # project imports
 from db import Unit
 
@@ -11,22 +12,25 @@ class UnitModule(object):
     def retrieve_units(self, agent_id):
         query = (Unit
                  .select(Unit, self.subunit_model)
-                 .where(Unit.agent_id == agent_id)
+                 .where(Unit.agent == agent_id)
                  .join(self.subunit_model)
-                 .order_by(Unit.log_time.desc()))
+                 .order_by(Unit.unix_timestamp.desc()))
         return query
 
     def datetime2unit(self, user_id):
         query = self.retrieve_units(user_id)
         dt2unit = collections.defaultdict(list)
         for unit in query:
-            dt2unit[unit.log_time].append(unit)
+            dt = helpers.get_datetime_from_unix_timestamp(unit.unix_timestamp)
+            dt2unit[dt].append(unit)
         return dt2unit
 
     def date2units(self, user_id):
         query = self.retrieve_units(user_id)
         date2units = collections.defaultdict(list)
         for unit in query:
-            date2units[unit.log_date].append(unit)
+            dt = helpers.get_datetime_from_unix_timestamp(unit.unix_timestamp)
+            d = helpers.get_bushido_date_from_datetime(dt)
+            date2units[d].append(unit)
         return date2units
 
