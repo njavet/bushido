24434591745a9d36b1a60069814689bd663d4224
Noe Javet <javetnoe@students.zhaw.ch>
2024-08-04 17:49:29 +0200

    rename screen

--- bushido/txscreens/category.py
+++ bushido/txscreens/category.py
@@ -2,12 +2,12 @@ from textual.app import ComposeResult
 from textual.screen import Screen
 
 
-class Lifting(Screen):
+class Category(Screen):
     BINDINGS = [('q', 'app.pop_screen', 'Back')]
 
-    def __init__(self, retriever):
+    def __init__(self, category):
         super().__init__()
-        self.ret = retriever
+        self.category = category
 
     def compose(self) -> ComposeResult:
         pass
