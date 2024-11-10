8dc646553d2077adff10ab1728a9d50d7811f221
Noe Javet <javetnoe@students.zhaw.ch>
2024-11-10 19:46:26 +0100

    remove obsolete test file, file renaming

--- bushido/keikolib/abscat.py
+++ bushido/keikolib/abscat.py
@@ -1,28 +1,7 @@
-import peewee as pw
 from typing import Dict
 from dataclasses import dataclass, field
 from collections import defaultdict
 from abc import ABC
-
-# project imports
-from bushido.keikolib.db import BaseModel, Unit, Message
-
-
-class AbsCategory(ABC):
-    def __init__(self, category: str):
-        self.category = category
-        self.keiko = None
-
-    def retrieve_units(self, uname=None) -> list:
-        query = (Unit
-                 .select(Unit, self.keiko)
-                 .join(self.keiko)
-                 .where(Unit.category == self.category)
-                 .order_by(Unit.timestamp.desc()))
-        if uname:
-            return [unit for unit in query if unit.uname == uname]
-        else:
-            return list(query)
 
 
 class AbsProcessor(ABC):
@@ -32,6 +11,7 @@ class AbsProcessor(ABC):
     def process_unit(self, unix_timestamp, words, comment):
         self._process_words(words)
         unit = self._save_unit(unix_timestamp)
+
         self._save_message(unit, words, comment)
         self._save_keiko(unit)
 
@@ -50,17 +30,3 @@ class AbsProcessor(ABC):
         Message.create(unit=unit,
                        payload=' '.join(words),
                        comment=comment)
-
-    def _save_keiko(self, unit):
-        raise NotImplementedError
-
-
-class Keiko(BaseModel):
-    unit = pw.ForeignKeyField(Unit)
-
-
-@dataclass
-class AbsUmojis:
-    umoji2uname: Dict[str, str]
-    # needed for different sized byte encodings of some emojis
-    emoji2umoji: defaultdict[dict] = field(default_factory=lambda: defaultdict(dict))
