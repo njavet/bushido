a246e395aa2f179ff9c57345da51b8991e8e7afb
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 15:32:37 +0100

    move to relative paths

--- bushido/db/models/__init__.py
+++ bushido/db/models/__init__.py
@@ -1,16 +1,14 @@
-from bushido.db.models.base import (Category,
-                                    Emoji,
-                                    Unit,
-                                    Message)
-from bushido.db.models.cardio import Cardio
-from bushido.db.models.chrono import Chrono
-from bushido.db.models.gym import Gym
-from bushido.db.models.lifting import Lifting, LSet
-from bushido.db.models.log import Log
-from bushido.db.models.mind import Mind
-from bushido.db.models.scale import Scale
-from bushido.db.models.wimhof import Wimhof, WimhofRound
+from .base import Category, Emoji, Unit, Message
+from .cardio import Cardio
+from .chrono import Chrono
+from .gym import Gym
+from .lifting import Lifting, LSet
+from .log import Log
+from .mind import Mind
+from .scale import Scale
+from .wimhof import Wimhof, WimhofRound
 
 # import Base for creating tables, needs to come after the
 # imports of the other tables.
-from bushido.db.models.base import Base
+from .base import Base
+
