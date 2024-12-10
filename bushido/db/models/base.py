dedb015562d31d5184e8a0f502ca7092328159bb
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 15:28:47 +0100

    remove obsolute nullable statements

--- bushido/db/models/base.py
+++ bushido/db/models/base.py
@@ -13,18 +13,18 @@ class Base(DeclarativeBase):
 
 class Category(Base):
     __tablename__ = 'category'
-    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(unique=True)
 
     emoji: Mapped['Emoji'] = relationship(back_populates='categories')
 
 
 class Emoji(Base):
     __tablename__ = 'emoji'
-    emoji_base: Mapped[str] = mapped_column(unique=True, nullable=False)
+    emoji_base: Mapped[str] = mapped_column(unique=True)
     emoji_ext: Mapped[Optional[str]] = mapped_column()
-    emoji_name: Mapped[str] = mapped_column(unique=True, nullable=False)
-    unit_name: Mapped[str] = mapped_column(unique=True, nullable=False)
-    category: Mapped[str] = mapped_column(ForeignKey(Category.name), nullable=False)
+    emoji_name: Mapped[str] = mapped_column(unique=True)
+    unit_name: Mapped[str] = mapped_column(unique=True)
+    category: Mapped[str] = mapped_column(ForeignKey(Category.name))
 
     categories: Mapped['Category'] = relationship(back_populates='emoji')
     units: Mapped['Unit'] = relationship(back_populates='emojis')
@@ -32,8 +32,8 @@ class Emoji(Base):
 
 class Unit(Base):
     __tablename__ = 'unit'
-    unix_timestamp: Mapped[float] = mapped_column(nullable=False)
-    emoji: Mapped[str] = mapped_column(ForeignKey(Emoji.unit_name), nullable=False)
+    unix_timestamp: Mapped[float] = mapped_column()
+    emoji: Mapped[str] = mapped_column(ForeignKey(Emoji.unit_name))
 
     emojis: Mapped['Emoji'] = relationship(back_populates='units')
     messages: Mapped['Message'] = relationship(back_populates='units',
@@ -44,11 +44,13 @@ class Message(Base):
     __tablename__ = 'message'
     payload: Mapped[str] = mapped_column()
     comment: Mapped[Optional[str]] = mapped_column()
-    unit: Mapped[int] = mapped_column(ForeignKey(Unit.key), nullable=False)
+    unit: Mapped[int] = mapped_column(ForeignKey(Unit.key))
+
     units: Mapped['Unit'] = relationship(back_populates='messages',
                                          cascade='all, delete-orphan')
 
 
 class Keiko(Base):
     __abstract__ = True
-    unit: Mapped[int] = mapped_column(ForeignKey(Unit.key), nullable=False)
+    unit: Mapped[int] = mapped_column(ForeignKey(Unit.key))
+
