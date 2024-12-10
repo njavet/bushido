2e1e594151774df1474f164e2c12de8d3cf2c9d4
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 19:46:00 +0100

    move processors

--- tests/test_db_integration.py
+++ tests/test_db_integration.py
@@ -1,28 +1,20 @@
 import unittest
-import peewee as pw
 
 # project imports
-from bushido.keikolib.db import Unit, Message
+from bushido.db import DatabaseManager
 
 
-test_db = pw.SqliteDatabase(':memory:')
-models = [Unit, Message]
-
-
-class TestInitDatabaseIntegration(unittest.TestCase):
-    def setUp(self):
-        test_db.bind(models)
-        test_db.connect()
-        test_db.create_tables(models)
-
-    def tearDown(self):
-        test_db.drop_tables(models)
-        test_db.close()
+class TestBaseDataIntegration(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dbm = DatabaseManager('sqlite:///:memory:')
+        dbm.init_tables()
+        dbm.upload_category_data()
+        dbm.upload_emoji_data()
+        cls.dbm = dbm
 
     def test_init_database(self):
-        self.assertTrue(Unit.table_exists())
-        self.assertTrue(Message.table_exists())
-        self.assertFalse(test_db.is_closed())
+        pass
 
 
 if __name__ == '__main__':
