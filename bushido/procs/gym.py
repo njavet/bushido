8b1bfee1a864a5febd8c995939e6198504799125
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 20:53:08 +0100

    join queries

--- bushido/procs/gym.py
+++ bushido/procs/gym.py
@@ -14,8 +14,8 @@ class UnitProcessor(AbsUnitProcessor):
 
     @dataclass
     class Attrs:
-        start_t: datetime.time
-        end_t: datetime.time
+        start_t: float
+        end_t: float
         gym: str
         training: str | None = field(init=False)
 
@@ -23,13 +23,24 @@ class UnitProcessor(AbsUnitProcessor):
             self.training = training
 
     def _process_words(self, words: list[str]) -> None:
+        today = datetime.date.today()
         start_t, end_t = parse_start_end_time_string(words[0])
+        start_dt = datetime.datetime(today.year,
+                                     today.month,
+                                     today.day,
+                                     start_t.hour,
+                                     start_t.minute)
+        end_dt = datetime.datetime(today.year,
+                                   today.month,
+                                   today.day,
+                                   end_t.hour,
+                                   end_t.minute)
         try:
             gym = words[1]
         except IndexError:
             raise ValueError('no gym')
 
-        self.attrs = self.Attrs(start_t, end_t, gym)
+        self.attrs = self.Attrs(start_dt.timestamp(), end_dt.timestamp(), gym)
         try:
             training = words[2]
         except IndexError:
