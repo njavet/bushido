65b81cc5e5b954d649ca92b509e980d26eaf6cb2
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 18:17:51 +0100

    wip

---
+++ bushido/services/abs_unit_proc.py
@@ -0,0 +1,41 @@
+from abc import ABC
+from sqlalchemy.orm import Session
+
+# project imports
+from bushido.db.models import Unit, Message
+
+
+class AbsUnitProcessor(ABC):
+    def __init__(self, engine):
+        self.engine = engine
+        self.attrs = None
+
+    def process_unit(self, unix_timestamp, words, comment, emoji_key) -> str:
+        try:
+            self._process_words(words)
+        except ValueError:
+            return 'wrong format'
+        unit_key = self._upload_unit(unix_timestamp, emoji_key)
+        payload = ' '.join(words)
+        self._upload_message(payload, comment, unit_key)
+        self._upload_keiko(unit_key)
+        return 'Unit confirmed'
+
+    def _process_words(self, words: list[str]) -> None:
+        raise NotImplementedError
+
+    def _upload_unit(self, unix_timestamp, emoji_key) -> int:
+        unit = Unit(unix_timestamp=unix_timestamp, emoji=emoji_key)
+        with Session(self.engine) as session:
+            session.add(unit)
+            session.commit()
+        return unit.key
+
+    def _upload_message(self, payload, comment, unit_key) -> None:
+        msg = Message(payload=payload, comment=comment, unit=unit_key)
+        with Session(self.engine) as session:
+            session.add(msg)
+            session.commit()
+
+    def _upload_keiko(self, unit_key):
+        raise NotImplementedError
---
+++ bushido/services/unit_manager.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+import importlib.util
+import inspect
+import os
+
+
+class UnitManager:
+    def __init__(self) -> None:
+        self.emoji2key: dict = {}
+        self.emoji2proc: dict = {}
+        # retrievers
+        self._load_categories()
+
+    def _load_categories(self):
+        categories = Path('bushido/keikolib')
+        # all file_path in categories should be valid category implementations
+        db_models = []
+        for module_path in categories.rglob('cat_*.py'):
+            module_name = module_path.stem[4:]
+            spec = importlib.util.spec_from_file_location(module_name, module_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+
+            keiko_name = module_name.capitalize()
+            keiko = [member for member in inspect.getmembers(module)
+                     if inspect.isclass(member[1]) and member[0] == keiko_name][0][1]
+            db_models.append(keiko)
+
+            self.categories[module_name] = module.Category(module_name)
+            self._load_processors(module_name, module)
+            self._load_incomplete_emojis(module)
+
+        data_dir = os.path.join(os.path.expanduser('~'), '.local/share/bushido')
+        db_url = os.path.join(data_dir, 'keiko.db')
+        init_database(db_url, db_models)
+
+    def _load_processors(self, category, module) -> None:
+        for umoji, uname in module.Umojis.umoji2uname.items():
+            self.umoji2proc[umoji] = module.Processor(category, uname, umoji)
+
+    def _load_incomplete_emojis(self, module) -> None:
+        try:
+            for emoji, umoji in module.Umojis.emoji2umoji.items():
+                self.emoji2umoji[emoji] = umoji
+        except AttributeError:
+            pass
+
+    def process_input(self, unix_timestamp, input_str):
+        try:
+            emoji, words, comment = self._preprocess_string(input_str)
+        except ValueError as err:
+            return str(err)
+        try:
+            emoji_key = self.emoji2key[emoji]
+            processor = self.emoji2proc[emoji]
+        except KeyError:
+            return 'Unknown emoji'
+        res = processor.process_unit(unix_timestamp, words, comment, emoji_key)
+        return res
+
+    @staticmethod
+    def _preprocess_string(input_str: str):
+        parts = input_str.split('//', 1)
+        emoji_payload = parts[0]
+        if not emoji_payload:
+            raise ValueError('Empty payload')
+        if len(parts) > 1 and parts[1]:
+            comment = parts[1].strip()
+        else:
+            comment = None
+        all_words = emoji_payload.split()
+        emoji = all_words[0]
+        words = all_words[1:]
+        return emoji, words, comment
