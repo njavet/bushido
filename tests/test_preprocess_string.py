d7b3894718b27fef60c34c59810cf2157c3e6109
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 19:57:32 +0100

    test update

--- tests/test_preprocess_string.py
+++ tests/test_preprocess_string.py
@@ -1,56 +1,62 @@
 import unittest
+from unittest.mock import Mock
 
 # project imports
-from bushido.keikolib.filters import preprocess_string
+from bushido.services.unit_manager import UnitManager
 
 
 class TestPreprocess(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        mock_dbm = Mock()
+        cls.um = UnitManager(mock_dbm)
+
     def test_empty_input_string(self):
         input_string = ''
         with self.assertRaises(ValueError):
-            preprocess_string(input_string)
+            self.um._preprocess_string(input_string)
 
     def test_comment_symbol_only(self):
         input_string = '//'
         with self.assertRaises(ValueError):
-            preprocess_string(input_string)
+            self.um._preprocess_string(input_string)
 
     def test_empty_payload(self):
         input_string = '// this is a comment'
         with self.assertRaises(ValueError):
-            preprocess_string(input_string)
+            self.um._preprocess_string(input_string)
 
     def test_correct_input_without_emoji_no_comment(self):
         input_string = '<emoji> some 101 char 5'
-        emoji, words, comment = preprocess_string(input_string)
+        emoji, words, comment = self.um._preprocess_string(input_string)
         self.assertEqual(emoji, '<emoji>')
         self.assertEqual(words, ['some', '101', 'char', '5'])
         self.assertIsNone(comment)
 
     def test_correct_input_without_emoji_no_comment_but_symbol(self):
         input_string = '<emoji> 101 5 //'
-        emoji, words, comment = preprocess_string(input_string)
+        emoji, words, comment = self.um._preprocess_string(input_string)
         self.assertEqual(emoji, '<emoji>')
         self.assertEqual(words, ['101', '5'])
         self.assertIsNone(comment)
 
     def test_correct_input_without_emoji_full(self):
         input_string = '<emoji> some numbers eg 5 // this is a comment'
-        emoji, words, comment = preprocess_string(input_string)
+        emoji, words, comment = self.um._preprocess_string(input_string)
         self.assertEqual(emoji, '<emoji>')
         self.assertEqual(words, ['some', 'numbers', 'eg', '5'])
         self.assertEqual(comment, 'this is a comment')
 
     def test_correct_input_without_emoji_no_payload(self):
         input_string = '<emoji> // this is a comment'
-        emoji, words, comment = preprocess_string(input_string)
+        emoji, words, comment = self.um._preprocess_string(input_string)
         self.assertEqual(emoji, '<emoji>')
         self.assertEqual(words, [])
         self.assertEqual(comment, 'this is a comment')
 
     def test_correct_input_without_emoji_no_payload_no_comment(self):
         input_string = '<emoji>'
-        emoji, words, comment = preprocess_string(input_string)
+        emoji, words, comment = self.um._preprocess_string(input_string)
         self.assertEqual(emoji, '<emoji>')
         self.assertEqual(words, [])
         self.assertIsNone(comment)
