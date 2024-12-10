723c386b6ae4e5a74da1e9e2c083f87284225bf7
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-10 17:28:52 +0100

    add utils

--- bushido/db/models/__init__.py
+++ bushido/db/models/__init__.py
@@ -2,13 +2,12 @@ from .base import Category, Emoji, Unit, Message
 from .cardio import Cardio
 from .chrono import Chrono
 from .gym import Gym
-from .lifting import Lifting, LSet
+from .lifting import Lifting
 from .log import Log
 from .mind import Mind
 from .scale import Scale
-from .wimhof import Wimhof, WimhofRound
+from .wimhof import Wimhof
 
 # import Base for creating tables, needs to come after the
 # imports of the other tables.
 from .base import Base
-
