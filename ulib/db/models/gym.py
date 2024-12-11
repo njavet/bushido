6f858e72155509a0df5bf98dd78def80784d53a1
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-11 19:50:59 +0100

    add database models

---
+++ ulib/db/models/gym.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from typing import Optional
+
+# project imports
+from .base import Keiko
+
+
+class Gym(Keiko):
+    __tablename__ = 'gym'
+
+    start_t: Mapped[float] = mapped_column()
+    end_t: Mapped[float] = mapped_column()
+    gym: Mapped[str] = mapped_column()
+    training: Mapped[Optional[str]] = mapped_column()
+
---
+++ ulib/db/models/log.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from typing import Optional
+
+# project imports
+from .base import Keiko
+
+
+class Log(Keiko):
+    __tablename__ = 'log'
+
+    log: Mapped[Optional[str]] = mapped_column()
