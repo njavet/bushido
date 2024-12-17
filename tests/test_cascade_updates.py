e7aac8e517e9004a90d5df484cab427b41bb6ae2
Noe Javet <javetnoe@students.zhaw.ch>
2024-12-17 18:09:25 +0100

    add delete cascade test

--- tests/test_cascade_updates.py
+++ tests/test_cascade_updates.py
@@ -1,11 +1,29 @@
 import unittest
+from sqlalchemy import select, update, delete
+from sqlalchemy.orm import Session
 
 
 # project imports
 from ulib.db import DatabaseManager
+from ulib.db.models import Category, Emoji, Unit
 
 
 class TestCategoryEmojiCascade(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        dbm = DatabaseManager('sqlite:///:memory:')
+        dbm = DatabaseManager('sqlite:///bushido.db')
+        dbm.init_tables([Category, Emoji, Unit])
+        dbm.upload_category_data()
+        dbm.upload_emoji_data()
+        cls.dbm = dbm
+
+    def test_cascade_delete_category(self):
+        stmt = select(Category).where(Category.name == 'lifting')
+        with Session(self.dbm.engine) as session:
+            category = session.scalar(stmt)
+            session.delete(category)
+            session.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
