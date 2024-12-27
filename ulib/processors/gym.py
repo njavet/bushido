d9d679f58dc0965d29400d61afe929b6eeb476e0
Noe Javet <np.javet@gmail.com>
2024-12-27 17:53:07 +0100

    refactoring

--- ulib/processors/gym.py
+++ ulib/processors/gym.py
@@ -1,16 +1,14 @@
 from dataclasses import dataclass, field
-from sqlalchemy.orm import Session
 import datetime
 
 # project imports
-from ulib.processors import AbsUnitProcessor
-from ulib.db.models import Gym
-from ulib.parsing.parsing import parse_start_end_time_string
+from .unit_processor_factory import UnitProcessorFactory
+from ulib.parsing import parse_start_end_time_string
 
 
-class UnitProcessor(AbsUnitProcessor):
-    def __init__(self, engine):
-        super().__init__(engine)
+class UnitProcessor(UnitProcessorFactory):
+    def __init__(self):
+        super().__init__()
 
     @dataclass
     class Attrs:
@@ -22,7 +20,7 @@ class UnitProcessor(AbsUnitProcessor):
         def set_optional_data(self, training):
             self.training = training
 
-    def _process_words(self, words: list[str]) -> None:
+    def process_words(self, words: list[str]) -> None:
         today = datetime.date.today()
         start_t, end_t = parse_start_end_time_string(words[0])
         start_dt = datetime.datetime(today.year,
@@ -47,14 +45,3 @@ class UnitProcessor(AbsUnitProcessor):
             training = None
 
         self.attrs.set_optional_data(training)
-
-    def _upload_keiko(self, unit_key):
-        gym = Gym(start_t=self.attrs.start_t,
-                  end_t=self.attrs.end_t,
-                  gym=self.attrs.gym,
-                  training=self.attrs.training,
-                  unit=unit_key)
-        with Session(self.engine) as session:
-            session.add(gym)
-            session.commit()
-
