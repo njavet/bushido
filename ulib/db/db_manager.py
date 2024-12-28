8855489358307558044eafe79318abf8eb51d93e
Noe Javet <np.javet@gmail.com>
2024-12-28 16:39:43 +0100

    db encapsulation

--- ulib/db/db_manager.py
+++ ulib/db/db_manager.py
@@ -5,20 +5,24 @@ import pandas as pd
 # project imports
 from ulib.db.base import Base, Category, Emoji
 from ulib.utils.emojis import create_emoji_dix
+from ulib.db.gym import GymUploader
+from ulib.db.lifting import LiftingUploader
 
 
 class DatabaseManager:
     def __init__(self, db_url):
         self.engine = create_engine(db_url)
         self.emoji_dix = create_emoji_dix(self.get_emojis())
+        self.uploaders = self.load_uploaders()
 
     @staticmethod
     def load_retrievers():
         pass
 
-    @staticmethod
-    def load_uploaders():
-        pass
+    def load_uploaders(self):
+        uploaders = {'gym': GymUploader(self.engine),
+                     'lifting': LiftingUploader(self.engine)}
+        return uploaders
 
     def init_db(self):
         self.init_tables()
