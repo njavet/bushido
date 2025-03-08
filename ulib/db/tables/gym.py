e8d393af457e8a0087d005759d4a1ce7e751ef1d
Noe Javet <np.javet@gmail.com>
2025-03-08 16:24:22 +0100

    tables

--- ulib/db/tables/gym.py
+++ ulib/db/tables/gym.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import mapped_column, Mapped, Session
+from sqlalchemy.orm import mapped_column, Mapped
 
 # project imports
-from ulib.db.tables.base import KeikoTable, BaseUploader
+from ulib.db.tables.base import KeikoTable
 
 
 class GymTable(KeikoTable):
@@ -10,19 +10,3 @@ class GymTable(KeikoTable):
     start_t: Mapped[float] = mapped_column()
     end_t: Mapped[float] = mapped_column()
     gym: Mapped[str] = mapped_column()
-
-
-class GymUploader(BaseUploader):
-    def __init__(self, engine):
-        super().__init__(engine)
-
-    def _upload_unit(self, attrs):
-        with Session(self.engine) as session:
-            session.add(self.unit)
-            session.commit()
-            keiko = GymTable(start_t=attrs.start_t,
-                             end_t=attrs.end_t,
-                             gym=attrs.gym,
-                             fk_unit=self.unit.key)
-            session.add(keiko)
-            session.commit()
