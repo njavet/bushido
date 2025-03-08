4666fbf4c0494d2bfad1db8dfc546950dddfedf5
Noe Javet <np.javet@gmail.com>
2025-03-08 18:52:40 +0100

    vue style components start

--- ulib/db/db_manager.py
+++ ulib/db/db_manager.py
@@ -2,17 +2,13 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 # project imports
-from ulib.db.tables import CategoryTable, EmojiTable
-from ulib.db.db_init import db_init
-from ulib.utils.emojis import create_emoji_dix
 from ulib.db.tables.gym import GymUploader
 from ulib.db.tables.lifting import LiftingUploader
 
 
-class DatabaseManager:
+class DBManager:
     def __init__(self, db_url):
         self.engine = create_engine(db_url)
-        db_init(self.engine)
         self.emoji_dix = create_emoji_dix(self.get_emojis())
         self.uploaders = self.load_uploaders()
 
--- ulib/db/tables/__init__.py
+++
@@ -1,3 +0,0 @@
-from .gym import GymTable
-from .lifting import LiftingTable
-from .base import CategoryTable, EmojiTable, UnitTable, MessageTable, Base
