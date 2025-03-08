acde2ced9bd6092b8e5216da8d5b2023ef3db167
Noe Javet <np.javet@gmail.com>
2025-03-08 18:59:14 +0100

    remove uploaders

--- ulib/categories/gym.py
+++ ulib/categories/gym.py
@@ -10,3 +10,24 @@ class GymTable(KeikoTable):
     start_t: Mapped[float] = mapped_column()
     end_t: Mapped[float] = mapped_column()
     gym: Mapped[str] = mapped_column()
+from sqlalchemy.orm import Session
+
+# project imports
+from ulib.db.tables.gym import GymTable
+from ulib.db.uploaders.base import BaseUploader
+
+
+class GymUploader(BaseUploader):
+    def __init__(self, engine):
+        super().__init__(engine)
+
+    def _upload_unit(self, attrs):
+        with Session(self.engine) as session:
+            session.add(self.unit)
+            session.commit()
+            keiko = GymTable(start_t=attrs.start_t,
+                             end_t=attrs.end_t,
+                             gym=attrs.gym,
+                             fk_unit=self.unit.key)
+            session.add(keiko)
+            session.commit()
