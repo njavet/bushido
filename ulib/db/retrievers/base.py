d3362df9211e0493ca083b0cdeb0f8eaba041afe
Noe Javet <np.javet@gmail.com>
2025-03-08 16:38:16 +0100

    retrievers

--- ulib/db/retrievers/base.py
+++ ulib/db/retrievers/base.py
@@ -2,8 +2,8 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 # project imports
-from ulib.db.tables.base import EmojiTable, CategoryTable
-from ulib.schemas.base import Emoji, Unit
+from ulib.db.tables.base import MDEmojiTable, MDCategoryTable, UnitTable
+from ulib.schemas.base import Emoji
 
 
 class BaseRetriever:
@@ -11,12 +11,43 @@ class BaseRetriever:
         self.engine = engine
 
     def get_emojis(self):
-        stmt = (select(EmojiTable.base_emoji,
-                       EmojiTable.ext_emoji,
-                       CategoryTable.name,
-                       EmojiTable.unit_name,
-                       EmojiTable.key)
-                .join(CategoryTable))
+        stmt = (select(MDEmojiTable.base_emoji,
+                       MDEmojiTable.ext_emoji,
+                       MDCategoryTable.name,
+                       MDEmojiTable.unit_name,
+                       MDEmojiTable.key)
+                .join(MDCategoryTable))
+        emoji_lst = []
         with Session(self.engine) as session:
-            emojis = session.execute(stmt).all()
-        return emojis
+            # TODO investigate open session for retrieving keys
+            #  -> not bound to a session error
+            data = session.execute(stmt).all()
+            for item in data:
+                byte_seq = item.base_emoji.encode('utf-8')
+                base_emoji = byte_seq.decode('unicode_escape')
+                if item.ext_emoji is None:
+                    emoji = base_emoji
+                else:
+                    bs = (item.base_emoji + item.ext_emoji).encode('utf-8')
+                    emoji = bs.decode('unicode_escape')
+                emoji_spec = Emoji(base_emoji=base_emoji,
+                                   emoji=emoji,
+                                   category_name=item.name,
+                                   unit_name=item.unit_name,
+                                   key=item.key)
+                emoji_lst.append(emoji_spec)
+        return emoji_lst
+
+    def create_emoji_dict(self) -> dict:
+        emoji_specs = self.get_emojis()
+        dix = {}
+        for emoji_spec in emoji_specs:
+            dix[emoji_spec.base_emoji] = emoji_spec
+            dix[emoji_spec.emoji] = emoji_spec
+        return dix
+
+    def get_units(self):
+        stmt = (select(UnitTable))
+        with Session(self.engine) as session:
+            units = session.scalars(stmt).all()
+        return units
