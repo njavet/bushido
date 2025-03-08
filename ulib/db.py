27254a2ea3d72a187b4782e2eca4d418cb529ab1
Noe Javet <np.javet@gmail.com>
2025-03-08 19:22:59 +0100

    abstract category

--- ulib/db.py
+++ ulib/db.py
@@ -1,8 +1,11 @@
 from typing import Optional
-from sqlalchemy import ForeignKey
+import pandas as pd
+from sqlalchemy import ForeignKey, select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import (DeclarativeBase,
                             Mapped,
-                            mapped_column)
+                            mapped_column,
+                            Session)
 
 
 class Base(DeclarativeBase):
@@ -35,119 +38,42 @@ class UnitTable(Base):
 class KeikoTable(Base):
     __abstract__ = True
     fk_unit: Mapped[int] = mapped_column(ForeignKey(UnitTable.key))
-from abc import ABC
-import pandas as pd
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
-
-# project imports
-from ulib.db.tables.base import Base, MDCategoryTable, MDEmojiTable, UnitTable
 
 
-class MDUploader:
-    def __init__(self, engine):
-        self.engine = engine
+def init_db(engine):
+    Base.metadata.create_all(engine)
+    try:
+        upload_category_data(engine)
+        upload_emoji_data(engine)
+    except IntegrityError:
+        # TODO logging
+        pass
 
-    def db_init(self):
-        Base.metadata.create_all(self.engine)
-        try:
-            self.upload_category_data()
-            self.upload_emoji_data()
-        except IntegrityError:
-            # TODO logging
-            pass
 
-    def upload_category_data(self):
-        categories = (pd
-                      .read_csv('ulib/resources/categories.csv')
-                      .to_dict(orient='records'))
-        cat_lst = [MDCategoryTable(name=cat['name']) for cat in categories]
-        with Session(self.engine) as session:
-            session.add_all(cat_lst)
-            session.commit()
-
-    def upload_emoji_data(self):
-        upload_lst = []
-        with Session(self.engine) as session:
-            categories = session.scalars(select(MDCategoryTable)).all()
-            cat_map = {cat.name: cat.key for cat in categories}
-            emojis_df = pd.read_csv('ulib/resources/emojis.csv')
-            emojis = emojis_df.to_dict(orient='records')
-            for emoji_data in emojis:
-                cat_key = cat_map[emoji_data['category_name']]
-                emoji = MDEmojiTable(base_emoji=emoji_data['base_emoji'],
-                                     ext_emoji=emoji_data['ext_emoji'],
-                                     emoji_name=emoji_data['emoji_name'],
-                                     unit_name=emoji_data['unit_name'],
-                                     fk_category=cat_key)
-            upload_lst.append(emoji)
-        session.add_all(upload_lst)
+def upload_category_data(engine):
+    categories = (pd
+                  .read_csv('ulib/resources/categories.csv')
+                  .to_dict(orient='records'))
+    cat_lst = [MDCategoryTable(name=cat['name']) for cat in categories]
+    with Session(engine) as session:
+        session.add_all(cat_lst)
         session.commit()
 
 
-class BaseUploader(ABC):
-    def __init__(self, engine):
-        self.engine = engine
-        self.unit = None
-
-    def upload_unit(self, timestamp, payload, comment, emoji_key, attrs):
-        self._create_unit(timestamp, payload, comment, emoji_key)
-        self._upload_unit(attrs)
-
-    def _create_unit(self, timestamp, payload, comment, emoji_key):
-        self.unit = UnitTable(timestamp=timestamp,
-                              payload=payload,
-                              comment=comment,
-                              fk_emoji=emoji_key)
-
-    def _upload_unit(self, attrs):
-        raise NotImplementedError
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-
-# project imports
-from ulib.db.tables.base import MDEmojiTable, MDCategoryTable, UnitTable
-from ulib.schemas.base import Emoji
-
-
-class BaseRetriever:
-    def __init__(self, engine):
-        self.engine = engine
-
-    def get_emojis(self):
-        stmt = (select(MDEmojiTable.base_emoji,
-                       MDEmojiTable.ext_emoji,
-                       MDCategoryTable.name,
-                       MDEmojiTable.unit_name,
-                       MDEmojiTable.key)
-                .join(MDCategoryTable))
-        emoji_lst = []
-        with Session(self.engine) as session:
-            # TODO investigate open session for retrieving keys
-            #  -> not bound to a session error
-            data = session.execute(stmt).all()
-            for item in data:
-                byte_seq = item.base_emoji.encode('utf-8')
-                base_emoji = byte_seq.decode('unicode_escape')
-                if item.ext_emoji is None:
-                    emoji = base_emoji
-                else:
-                    bs = (item.base_emoji + item.ext_emoji).encode('utf-8')
-                    emoji = bs.decode('unicode_escape')
-                emoji_spec = Emoji(base_emoji=base_emoji,
-                                   emoji=emoji,
-                                   category_name=item.name,
-                                   unit_name=item.unit_name,
-                                   key=item.key)
-                emoji_lst.append(emoji_spec)
-        return emoji_lst
-
-    def create_emoji_dict(self) -> dict:
-        emoji_specs = self.get_emojis()
-        dix = {}
-        for emoji_spec in emoji_specs:
-            dix[emoji_spec.base_emoji] = emoji_spec
-            dix[emoji_spec.emoji] = emoji_spec
-        return dix
-
+def upload_emoji_data(engine):
+    upload_lst = []
+    with Session(engine) as session:
+        categories = session.scalars(select(MDCategoryTable)).all()
+        cat_map = {cat.name: cat.key for cat in categories}
+        emojis_df = pd.read_csv('ulib/resources/emojis.csv')
+        emojis = emojis_df.to_dict(orient='records')
+        for emoji_data in emojis:
+            cat_key = cat_map[emoji_data['category_name']]
+            emoji = MDEmojiTable(base_emoji=emoji_data['base_emoji'],
+                                 ext_emoji=emoji_data['ext_emoji'],
+                                 emoji_name=emoji_data['emoji_name'],
+                                 unit_name=emoji_data['unit_name'],
+                                 fk_category=cat_key)
+            upload_lst.append(emoji)
+    session.add_all(upload_lst)
+    session.commit()
