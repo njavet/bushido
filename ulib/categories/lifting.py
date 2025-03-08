bb7bd7a210d09706fb6165bf7eb73fe48bced1d9
Noe Javet <np.javet@gmail.com>
2025-03-08 19:00:39 +0100

    remove parsers

--- ulib/categories/lifting.py
+++ ulib/categories/lifting.py
@@ -36,3 +36,44 @@ class LiftingUploader(BaseUploader):
                 keikos.append(lifting)
             session.add_all(keikos)
             session.commit()
+from dataclasses import dataclass
+
+# project imports
+from ulib.parsers.base_parser import BaseParser
+
+
+class LiftingParser(BaseParser):
+    def __init__(self):
+        super().__init__()
+
+    @dataclass
+    class Attrs:
+        sets: list[int]
+        weights: list[float]
+        reps: list[float]
+        pauses: list[int]
+
+        def zipped(self):
+            return zip(self.sets, self.weights, self.reps, self.pauses)
+
+    def parse_words(self, words) -> Attrs:
+        try:
+            weights = [float(w) for w in words[::3]]
+            reps = [float(r) for r in words[1::3]]
+            pauses = [int(p) for p in words[2::3]] + [0]
+        except ValueError:
+            raise ValueError('invalid input')
+
+        if len(reps) != len(weights):
+            raise ValueError(
+                'Not the same number of reps and weights')
+        if len(pauses) != len(reps):
+            raise ValueError('break error')
+        if len(reps) < 1:
+            raise ValueError('No set')
+
+        attrs = self.Attrs(sets=list(range(len(weights))),
+                           weights=weights,
+                           reps=reps,
+                           pauses=pauses)
+        return attrs
