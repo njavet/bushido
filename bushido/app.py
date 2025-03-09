f6d1e10637402a0ad19caef5771e1fb7fc929aac
Noe Javet <np.javet@gmail.com>
2025-03-09 17:48:49 +0100

    endpoints

--- bushido/app.py
+++ bushido/app.py
@@ -1,21 +1,12 @@
-import uvicorn
-from pathlib import Path
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 # project imports
+from bushido.api.endpoints import router
 
 
 def create_fastapi_app():
     app = FastAPI()
     app.mount('/static', StaticFiles(directory='static'), name='static')
-    templates = Jinja2Templates(directory='templates')
-
-    @app.get('/', response_class=HTMLResponse)
-    async def index(request: Request):
-        return templates.TemplateResponse('index.html',
-                                          {'request': request})
-
+    app.include_router(router)
     return app
