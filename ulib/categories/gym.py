3ff5003c5976760794024b0f040c5be2ae6d6171
Noe Javet <np.javet@gmail.com>
2025-03-09 11:43:08 +0100

    gym parsing

--- ulib/categories/gym.py
+++ ulib/categories/gym.py
@@ -1,7 +1,9 @@
+import datetime
 from sqlalchemy.orm import mapped_column, Mapped, Session
 
 # project imports
 from ulib.abs_category import AbsCategory, AbsProcessor, AbsKeikoTable
+from ulib.utils.parsing import parse_start_end_time_string
 
 
 class Category(AbsCategory):
@@ -14,33 +16,6 @@ class Processor(AbsProcessor):
         super().__init__(engine)
 
     def process_keiko(self, unit, words):
-        start_t, end_t = parse_start_end_time_string(words[0])
-        try:
-            gym = words[1]
-        except IndexError:
-            raise ValueError('no gym')
-
-        with Session(self.engine) as session:
-            session.add(unit)
-            session.commit()
-            keiko = KeikoTable(start_t=start_t,
-                               end_t=end_t,
-                               gym=gym,
-                               fk_unit=unit.key)
-            session.add(keiko)
-            session.commit()
-
-
-class KeikoTable(AbsKeikoTable):
-    __tablename__ = 'gym'
-
-    start_t: Mapped[float] = mapped_column()
-    end_t: Mapped[float] = mapped_column()
-    gym: Mapped[str] = mapped_column()
-
-
-
-    def parse_words(self, words: list[str]) -> Attrs:
         today = datetime.date.today()
         start_t, end_t = parse_start_end_time_string(words[0])
         start_dt = datetime.datetime(today.year,
@@ -58,11 +33,20 @@ class KeikoTable(AbsKeikoTable):
         except IndexError:
             raise ValueError('no gym')
 
-        attrs = self.Attrs(start_dt.timestamp(), end_dt.timestamp(), gym)
-        try:
-            training = words[2]
-        except IndexError:
-            training = None
+        with Session(self.engine) as session:
+            session.add(unit)
+            session.commit()
+            keiko = KeikoTable(start_t=start_dt.timestamp(),
+                               end_t=end_dt.timestamp(),
+                               gym=gym,
+                               fk_unit=unit.key)
+            session.add(keiko)
+            session.commit()
 
-        attrs.set_optional_data(training)
-        return attrs
+
+class KeikoTable(AbsKeikoTable):
+    __tablename__ = 'gym'
+
+    start_t: Mapped[float] = mapped_column()
+    end_t: Mapped[float] = mapped_column()
+    gym: Mapped[str] = mapped_column()
