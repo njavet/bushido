cda8bee75c17d6c2258051023a5f10d1f13a5524
Noe Javet <np.javet@gmail.com>
2025-03-09 18:26:33 +0100

    fix

--- unitlib/utils/helpers.py
+++ unitlib/utils/helpers.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 
 def load_csv(csv_file):
-    with importlib.resources.files('ulib.resources').joinpath(csv_file).open('r') as f:
+    with importlib.resources.files('unitlib.resources').joinpath(csv_file).open('r') as f:
         return pd.read_csv(f)
 
