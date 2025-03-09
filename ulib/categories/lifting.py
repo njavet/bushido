f1636607b778ff750b0c3acd34354c9fa5c4eda7
Noe Javet <np.javet@gmail.com>
2025-03-09 11:47:33 +0100

    lifting

--- ulib/categories/lifting.py
+++ ulib/categories/lifting.py
@@ -1,62 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, Session
 
 # project imports
-from ulib.db.tables.base import KeikoTable
+from ulib.abs_category import AbsCategory, AbsProcessor, AbsKeikoTable
 
 
-class LiftingTable(KeikoTable):
-    __tablename__ = 'lifting'
-
-    set_nr: Mapped[int] = mapped_column()
-    weight: Mapped[float] = mapped_column()
-    reps: Mapped[float] = mapped_column()
-    pause: Mapped[int] = mapped_column(default=0)
-from sqlalchemy.orm import Session
-
-# project imports
-from ulib.db.uploaders.base import BaseUploader
-from ulib.db.tables.lifting import LiftingTable
+class Category(AbsCategory):
+    def __init__(self, name, engine):
+        super().__init__(name, engine)
 
 
-class LiftingUploader(BaseUploader):
+class Processor(AbsProcessor):
     def __init__(self, engine):
         super().__init__(engine)
 
-    def _upload_unit(self, attrs):
-        with Session(self.engine) as session:
-            session.add(self.unit)
-            session.commit()
-            keikos = []
-            for set_nr, w, r, p in attrs.zipped():
-                lifting = LiftingTable(set_nr=set_nr,
-                                       weight=w,
-                                       reps=r,
-                                       pause=p,
-                                       fk_unit=self.unit.key)
-                keikos.append(lifting)
-            session.add_all(keikos)
-            session.commit()
-from dataclasses import dataclass
-
-# project imports
-from ulib.parsers.base_parser import BaseParser
-
-
-class LiftingParser(BaseParser):
-    def __init__(self):
-        super().__init__()
-
-    @dataclass
-    class Attrs:
-        sets: list[int]
-        weights: list[float]
-        reps: list[float]
-        pauses: list[int]
-
-        def zipped(self):
-            return zip(self.sets, self.weights, self.reps, self.pauses)
-
-    def parse_words(self, words) -> Attrs:
+    def process_keiko(self, unit, words):
         try:
             weights = [float(w) for w in words[::3]]
             reps = [float(r) for r in words[1::3]]
@@ -72,8 +29,25 @@ class LiftingParser(BaseParser):
         if len(reps) < 1:
             raise ValueError('No set')
 
-        attrs = self.Attrs(sets=list(range(len(weights))),
-                           weights=weights,
-                           reps=reps,
-                           pauses=pauses)
-        return attrs
+        with Session(self.engine) as session:
+            session.add(unit)
+            session.commit()
+            keikos = []
+            for set_nr, (w, r, p) in enumerate(zip(weights, reps, pauses)):
+                keiko = KeikoTable(set_nr=set_nr,
+                                   weight=w,
+                                   reps=r,
+                                   pause=p,
+                                   fk_unit=unit.key)
+                keikos.append(keiko)
+            session.add_all(keikos)
+            session.commit()
+
+
+class KeikoTable(AbsKeikoTable):
+    __tablename__ = 'lifting'
+
+    set_nr: Mapped[int] = mapped_column()
+    weight: Mapped[float] = mapped_column()
+    reps: Mapped[float] = mapped_column()
+    pause: Mapped[int] = mapped_column(default=0)
