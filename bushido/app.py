0fe52c9f5fe66e6153deaed9053881e949327549
Noe Javet <np.javet@gmail.com>
2025-03-13 14:46:28 +0100

    remove old makefile

--- bushido/app.py
+++ bushido/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from unitlib.unitlib import UnitManager
 
 # project imports
 from bushido.api.endpoints import router
 
 
 def create_fastapi_app():
+    um = UnitManager('sqlite:///bushido.db')
     app = FastAPI()
     app.mount('/static', StaticFiles(directory='static'), name='static')
     app.include_router(router)
