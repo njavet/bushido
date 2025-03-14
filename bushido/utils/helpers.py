bb0dba7ac54579f6746f810d6da23db8e6413076
Noe Javet <np.javet@gmail.com>
2025-03-14 15:18:48 +0100

    remove csv loader for outside package files

--- bushido/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-import importlib.resources
-import pandas as pd
-
-
-def load_csv(csv_file):
-    with importlib.resources.files('unitlib.resources').joinpath(csv_file).open('r') as f:
-        return pd.read_csv(f)
-
