a6ad5427426a9c60e24c5ab2fbd73e2dc4509cc9
Noe Javet <np.javet@gmail.com>
2025-03-14 19:33:41 +0100

    update gitignore

--- bushido/utils/emojis.py
+++ bushido/utils/emojis.py
@@ -2,11 +2,21 @@ from pathlib import Path
 import pandas as pd
 
 
-def prepare_emojis(csv_path=Path('ulib/resources/emojis.csv')):
+def extend_emoji_df(csv_path=Path('bushido/static/csv_files/emojis.csv')):
     emojis = pd.read_csv(csv_path)
     emojis['ext_emoji'] = emojis['ext_emoji'].fillna('')
-    emojis['emoji'] = emojis['base_emoji'] + emojis['ext_emoji']
-    emojis['emoji'] = emojis['emoji'].apply(
+    emojis['double'] = emojis['base_emoji'] + emojis['ext_emoji']
+    emojis['double'] = emojis['double'].apply(
         lambda x: x.encode('utf-8').decode('unicode_escape')
     )
+    emojis['single'] = emojis['base_emoji'].apply(
+        lambda x: x.encode('utf-8').decode('unicode_escape')
+    )
+    emojis['is_unique'] = emojis['double'] == emojis['single']
     return emojis
+
+
+def get_convert_dict(csv_path=Path('bushido/static/csv_files/emojis.csv')):
+    emojis = extend_emoji_df(csv_path=csv_path)
+    emojis = emojis.drop(columns=['ext_emoji', 'base_emoji'])
+    dix = {}
