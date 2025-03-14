988a201034a31b42d28510a2cf8e77a7347c7df5
Noe Javet <np.javet@gmail.com>
2025-03-14 14:50:07 +0100

    add unitlib

---
+++ tests/test_cascade_updates.py
@@ -0,0 +1,26 @@
+import unittest
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import Session
+
+
+# project imports
+from unitlib.db import MDCategoryTable, init_db
+
+
+class TestCategoryEmojiCascade(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        engine = create_engine('sqlite:///:memory:')
+        init_db(engine)
+        cls.engine = engine
+
+    def test_cascade_delete_category(self):
+        stmt = select(MDCategoryTable).where(MDCategoryTable.name == 'lifting')
+        with Session(self.engine) as session:
+            category = session.scalar(stmt)
+            session.delete(category)
+            session.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
