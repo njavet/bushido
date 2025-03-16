c456ef5098b8dc7e1a653efcdea18efa350ed2c1
Noe Javet <np.javet@gmail.com>
2025-03-16 11:24:28 +0100

    front end

--- bushido/services/display.py
+++ bushido/services/display.py
@@ -1,8 +1,12 @@
+import pytz
+from datetime import datetime
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 # project imports
 from bushido.db.base_tables import MDEmojiTable, UnitTable
+from bushido.schemas.base import UnitDisplay
+from bushido.utils.emojis import combine_emoji
 
 
 class DisplayService:
@@ -13,13 +17,23 @@ class DisplayService:
         stmt = (select(MDEmojiTable.base_emoji,
                        MDEmojiTable.ext_emoji,
                        UnitTable.timestamp,
-                       UnitTable.payload)
+                       UnitTable.payload,
+                       UnitTable.comment)
                 .join(UnitTable)
                 ).order_by(UnitTable.timestamp)
         with Session(self.dbm.engine) as session:
             result = session.execute(stmt).all()
+        lst = []
+        zurich = pytz.timezone('Europe/Zurich')
+        for unit in result:
+            dt_utc = datetime.fromtimestamp(unit.timestamp, tz=pytz.utc)
+            ud = UnitDisplay(dt=dt_utc.astimezone(zurich),
+                             emoji=combine_emoji(unit.base_emoji, unit.ext_emoji),
+                             payload=unit.payload,
+                             comment=unit.comment)
+            lst.append(ud)
 
-        return result
+        return lst
 
 
 
