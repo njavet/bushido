94bd0015cf6a2ec5c4edc3a1e4e16c36140fecf7
Noe Javet <np.javet@gmail.com>
2025-03-16 11:44:47 +0100

    day template

--- bushido/services/display.py
+++ bushido/services/display.py
@@ -13,27 +13,5 @@ class DisplayService:
     def __init__(self, dbm):
         self.dbm = dbm
 
-    def get_units(self):
-        stmt = (select(MDEmojiTable.base_emoji,
-                       MDEmojiTable.ext_emoji,
-                       UnitTable.timestamp,
-                       UnitTable.payload,
-                       UnitTable.comment)
-                .join(UnitTable)
-                ).order_by(UnitTable.timestamp)
-        with Session(self.dbm.engine) as session:
-            result = session.execute(stmt).all()
-        lst = []
-        zurich = pytz.timezone('Europe/Zurich')
-        for unit in result:
-            dt_utc = datetime.fromtimestamp(unit.timestamp, tz=pytz.utc)
-            ud = UnitDisplay(dt=dt_utc.astimezone(zurich),
-                             emoji=combine_emoji(unit.base_emoji, unit.ext_emoji),
-                             payload=unit.payload,
-                             comment=unit.comment)
-            lst.append(ud)
-
-        return lst
-
 
 
