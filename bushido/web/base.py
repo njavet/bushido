cdd7f6c8e74d6d5c769a978568624d85bd2708c1
Noe Javet <np.javet@gmail.com>
2025-03-23 12:39:50 +0100

    emoji storage

--- bushido/web/base.py
+++ bushido/web/base.py
@@ -9,8 +9,7 @@ from fastapi.responses import HTMLResponse
 router = APIRouter()
 templates = Jinja2Templates(directory='templates/')
 
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tributejs@5.1.3/dist/tribute.css">
-  <script src="https://cdn.jsdelivr.net/npm/tributejs@5.1.3/dist/tribute.min.js"></script>
+
 @router.get('/', response_class=HTMLResponse)
 async def get_index(request: Request):
     data = await request.body()
