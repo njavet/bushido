4b8e9eb703a995410cb0b73d2847f42bd6b25d79
Noe Javet <np.javet@gmail.com>
2025-03-23 21:02:48 +0100

    wip

--- bushido/web/base.py
+++ bushido/web/base.py
@@ -1,22 +1,29 @@
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 # project imports
+from bushido.data.base import DatabaseManager
 from bushido.service.base import BaseService
 
 
 # TODO investigate global variables
 router = APIRouter()
 templates = Jinja2Templates(directory='templates/')
-print('create')
-base_service = BaseService()
+
+
+@router.get('/emojis')
+async def fetch_emojis(dbm: DatabaseManager = Depends(dbm)):
+    pass
+
 
 
 @router.get('/', response_class=HTMLResponse)
 async def get_index(request: Request):
+    dix = base_service.construct_autocomplete_dix()
     return templates.TemplateResponse('index.html',
-                                      {'request': request})
+                                      {'request': request,
+                                       'autocomp': dix})
 
 
 @router.post('/log_unit')
