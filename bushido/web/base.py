076e35df208b43a25f2cdd8857169df53876f6e9
Noe Javet <np.javet@gmail.com>
2025-03-23 12:11:38 +0100

    tribute for autocompletion

--- bushido/web/base.py
+++ bushido/web/base.py
@@ -9,14 +9,17 @@ from fastapi.responses import HTMLResponse
 router = APIRouter()
 templates = Jinja2Templates(directory='templates/')
 
-
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tributejs@5.1.3/dist/tribute.css">
+  <script src="https://cdn.jsdelivr.net/npm/tributejs@5.1.3/dist/tribute.min.js"></script>
 @router.get('/', response_class=HTMLResponse)
 async def get_index(request: Request):
+    data = await request.body()
+    print('d', data)
     return templates.TemplateResponse('index.html',
                                       {'request': request})
 
 
 @router.post('/log_unit')
 async def log_unit(request: Request):
-    pass
-
+    data = await request.json()
+    print('data', data)
