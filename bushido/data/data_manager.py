c69ab984cf7f34b02ee21b5bd4afe8d0af47fdef
Noe Javet <np.javet@gmail.com>
2025-03-28 15:48:47 +0100

    wip

--- bushido/data/data_manager.py
+++ bushido/data/data_manager.py
@@ -4,46 +4,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
 # project imports
-from bushido.data.db import MDEmojiTable, MDCategoryTable, UnitTable, Base
-from bushido.model.base import EmojiSpec
+from bushido.data.db import MDEmojiTable, MDCategoryTable, Base
 
 
 class DataManager:
     def __init__(self, db_url) -> None:
         self.engine = create_engine(url=db_url)
         self.init_db()
-
-    def load_emojis(self):
-        stmt = (select(MDEmojiTable.unit_name,
-                       MDEmojiTable.emoji,
-                       MDEmojiTable.emoji_text,
-                       MDCategoryTable.name,
-                       MDEmojiTable.key)
-                .join(MDCategoryTable))
-        emoji_specs = []
-        with Session(self.engine) as session:
-            # TODO investigate open session for retrieving keys
-            #  -> not bound to a session error
-            data = session.execute(stmt).all()
-        for item in data:
-            emoji_spec = EmojiSpec(emoji=item.emoji,
-                                   emoji_text=item.emoji_text,
-                                   unit_name=item.unit_name,
-                                   category_name=item.name,
-                                   key=item.key)
-            emoji_specs.append(emoji_spec)
-        return emoji_specs
-
-    def retrieve_all_units(self):
-        stmt = (select(MDEmojiTable.emoji,
-                       UnitTable.timestamp,
-                       UnitTable.payload,
-                       UnitTable.comment)
-                .join(UnitTable)
-                ).order_by(UnitTable.timestamp.desc())
-        with Session(self.engine) as session:
-            units = session.execute(stmt).all()
-        return units
 
     def init_db(self):
         Base.metadata.create_all(self.engine)
