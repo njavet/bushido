778932f1a47a4a50a9af904a7ee049e22b21adf5
Noe Javet <np.javet@gmail.com>
2025-04-12 15:17:45 +0200

    poetry setup / structure

---
+++ bushido/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+# project imports
+from bushido.conf import DEFAULT_PORT
+from bushido.web import router
+
+
+def create_app():
+    app = FastAPI()
+
+    app.mount('/bushido/static',
+              StaticFiles(directory='bushido/static'), name='static')
+    app.state.dm = None
+
+    app.include_router(router)
+
+    return app
+
+
+def run_app():
+    uvicorn.run('bushido.main:create_app',
+                port=DEFAULT_PORT,
+                reload=True,
+                factory=True,
+                log_level='debug')
+
+
+if __name__ == '__main__':
+    run_app()
