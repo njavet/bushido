e6b6bc0a089917dca1a2a65fe5d3a3e4e94f2fac
Noe Javet <np.javet@gmail.com>
2025-04-12 15:46:33 +0200

    move db_init code

--- bushido/conf.py
+++ bushido/conf.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+MASTER_DATA_DIR = Path('bushido', 'static', 'master_data')
 DEFAULT_PORT = 8080
 
 DAY_START_HOUR = 4
---
+++ bushido/data/db_init.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+# project imports
+from bushido.data.base_tables import MDEmojiTable, MDCategoryTable, Base
+
+
+def init_db(engine):
+    Base.metadata.create_all(engine)
+    try:
+        upload_category_md_data(engine)
+        upload_emoji_md_data(engine)
+    except IntegrityError:
+        # TODO logging
+        pass
+
+
+def upload_category_md_data(engine):
+    categories = pd.read_csv('static/csv_files/categories.csv')
+    categories = categories.to_dict(orient='records')
+    cat_lst = [MDCategoryTable(name=cat['name']) for cat in categories]
+    with Session(engine) as session:
+        session.add_all(cat_lst)
+        session.commit()
+
+
+def upload_emoji_md_data(engine):
+    emojis = pd.read_csv('static/csv_files/emojis.csv')
+    emojis = emojis.to_dict(orient='records')
+    upload_lst = []
+    with Session(engine) as session:
+        categories = session.scalars(select(MDCategoryTable)).all()
+        cat_map = {cat.name: cat.key for cat in categories}
+        for emoji_data in emojis:
+            cat_key = cat_map[emoji_data['category_name']]
+            emoji = MDEmojiTable(unit_name=emoji_data['unit_name'],
+                                 emoji_name=emoji_data['emoji_name'],
+                                 emoji_text=emoji_data['emoji_text'],
+                                 emoji=emoji_data['emoji'],
+                                 fk_category=cat_key)
+            upload_lst.append(emoji)
+    session.add_all(upload_lst)
+    session.commit()
