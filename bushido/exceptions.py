0f025fd9771c2dd9b51c781a2bdb8923be1e8fb3
Noe Javet <np.javet@gmail.com>
2025-04-12 15:22:08 +0200

    web interface, exceptions

---
+++ bushido/exceptions.py
@@ -0,0 +1,10 @@
+class ValidationError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
+class UploadError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
--- bushido/web/base.py
+++ bushido/web/base.py
@@ -1,34 +1,15 @@
-from fastapi import Request, APIRouter, Depends
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import Request, APIRouter
+from fastapi.responses import HTMLResponse
 
 # project imports
-from bushido.service.base import BaseService
-from bushido.service.categories.category import InputProcessor
+from bushido.service.setup import setup_dm
 
 
-# TODO investigate global variables
 router = APIRouter()
-templates = Jinja2Templates(directory='templates/')
-bs = BaseService()
-ip = InputProcessor(bs.load_processors())
-
-
-@router.get('/emojis')
-async def fetch_emojis():
-    return bs.construct_autocomplete_list()
 
 
 @router.get('/', response_class=HTMLResponse)
-async def get_index(request: Request):
-    return templates.TemplateResponse('index.html',
-                                      {'request': request})
+async def index(request: Request):
+    templates = request.app.state.templates
+    return templates.TemplateResponse('index.html', {'request': request})
 
-
-@router.post('/log_unit')
-async def log_unit(request: Request):
-    data = await request.json()
-    print('data', data)
-    answer = ip.preprocess_input(data['text'])
-    print('answer', answer)
-    return {'answer': answer}
