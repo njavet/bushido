73e84f548fda0c1246dd2681c7d0114cec8e696d
Noe Javet <np.javet@gmail.com>
2025-04-13 20:11:47 +0200

    change fastapi endpoints

--- bushido/web/__init__.py
+++ bushido/web/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
 # project imports
-from .base import router as base_router
 from .units import router as unit_router
 
 router = APIRouter()
-router.include_router(base_router)
 router.include_router(unit_router)
