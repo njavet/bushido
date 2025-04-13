5947e7bf0757e56575716fc61a3403cb13066891
Noe Javet <np.javet@gmail.com>
2025-04-13 15:29:40 +0200

    emojis decode

--- bushido/service/setup.py
+++ bushido/service/setup.py
@@ -1,8 +1,15 @@
 
 # project imports
+from bushido.conf import DB_URL
 from bushido.data.manager import DataManager
+from bushido.service.units import UnitProcessor
 
 
-def setup_dm(db_url):
-    dm = DataManager(db_url)
+def setup_dm():
+    dm = DataManager(db_url=DB_URL)
     return dm
+
+
+def setup_up(dm):
+    up = UnitProcessor(dm)
+    return up
--- bushido/utils/emojis.py
+++ bushido/utils/emojis.py
@@ -1,8 +1,11 @@
+import pandas as pd
+import ast
+
+def decode_unicode_emoji(code):
+    return ast.literal_eval(f"'{code}'") if pd.notnull(code) else ''
 
 
-def construct_emoji(emoji_base, emoji_ext):
-    if emoji_ext is None:
-        emoji = emoji_base.encode().decode('unicode-escape')
-    else:
-        emoji = (emoji_base + emoji_ext).encode().decode('unicode-escape')
-    return emoji
+def decode(df):
+    df['emoticon'] = df['emoji_base'].apply(decode_unicode_emoji)
+    df['emoji'] = df['emoticon'] + df['emoji_ext'].apply(decode_unicode_emoji)
+    return df.drop(columns=['emoji_base', 'emoji_ext'])
