e2fe334b9e7769ff9bb702a59c3b48c529d16cb2
Noe Javet <np.javet@gmail.com>
2025-04-13 16:19:03 +0200

    load keiko processors

--- bushido/schema/base.py
+++ bushido/schema/base.py
@@ -1,9 +1,8 @@
-# is it good practice to use the db key in the model ?
 from pydantic import BaseModel
 
 
 class UnitSpec(BaseModel):
     timestamp: int
     unit_name: str
-    payload: str
+    words: list[str]
     comment: str | None = None
--- bushido/service/categories/gym.py
+++ bushido/service/categories/gym.py
@@ -5,32 +5,42 @@ import datetime
 from bushido.exceptions import ValidationError
 from bushido.schema.gym import KeikoSpec
 from bushido.utils.parsing import parse_start_end_time_string
+from bushido.data.categories.gym import create_keiko_orm
 
 
-def parse_unit(words):
-    today = datetime.date.today()
-    start_t, end_t = parse_start_end_time_string(words[0])
-    start_dt = datetime.datetime(today.year,
-                                 today.month,
-                                 today.day,
-                                 start_t.hour,
-                                 start_t.minute)
-    start_dt = start_dt.replace(tzinfo=ZoneInfo('Europe/Zurich'))
-    start_ts = int(start_dt.timestamp())
+class KeikoProcessor:
+    def __init__(self):
+        pass
 
-    end_dt = datetime.datetime(today.year,
-                               today.month,
-                               today.day,
-                               end_t.hour,
-                               end_t.minute)
-    end_dt = end_dt.replace(tzinfo=ZoneInfo('Europe/Zurich'))
-    end_ts = int(end_dt.timestamp())
-    try:
-        gym = words[1]
-    except IndexError:
-        raise ValidationError('no gym')
+    def process_keiko(self, words):
+        keiko_spec = self.parse_keiko(words)
+        return create_keiko_orm(keiko_spec)
 
-    keiko_spec = KeikoSpec(start_t=start_ts,
-                           end_t=end_ts,
-                           gym=gym)
-    return keiko_spec
+    @staticmethod
+    def parse_keiko(words):
+        today = datetime.date.today()
+        start_t, end_t = parse_start_end_time_string(words[0])
+        start_dt = datetime.datetime(today.year,
+                                     today.month,
+                                     today.day,
+                                     start_t.hour,
+                                     start_t.minute)
+        start_dt = start_dt.replace(tzinfo=ZoneInfo('Europe/Zurich'))
+        start_ts = int(start_dt.timestamp())
+
+        end_dt = datetime.datetime(today.year,
+                                   today.month,
+                                   today.day,
+                                   end_t.hour,
+                                   end_t.minute)
+        end_dt = end_dt.replace(tzinfo=ZoneInfo('Europe/Zurich'))
+        end_ts = int(end_dt.timestamp())
+        try:
+            gym = words[1]
+        except IndexError:
+            raise ValidationError('no gym')
+
+        keiko_spec = KeikoSpec(start_t=start_ts,
+                               end_t=end_ts,
+                               gym=gym)
+        return keiko_spec
