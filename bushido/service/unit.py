58d6ff909051df6301c66a1f63198bacb2fddbc7
Noe Javet <np.javet@gmail.com>
2025-04-13 22:09:59 +0200

    remove category

--- bushido/service/unit.py
+++ bushido/service/unit.py
@@ -9,6 +9,9 @@ class UnitService:
     def get_units(self, unit_name=None, start_t=None, end_t=None):
         return self.unit_repo.get_units(unit_name, start_t, end_t)
 
+    def get_category_for_unit(self, unit_name):
+        return self.unit_repo.get_category_for_unit(unit_name)
+
     def create_unit(self, spec):
         emoji_key = self.emoji_repo.get_emoji_key_by_unit(spec.unit_name)
         unit = UnitTable(timestamp=spec.timestamp,
