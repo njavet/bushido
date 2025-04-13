9e3d8b969b952a692afd2beb1e77ffa53d21fe4b
Noe Javet <np.javet@gmail.com>
2025-04-13 17:01:10 +0200

    scale schema

---
+++ bushido/service/categories/scale.py
@@ -0,0 +1,59 @@
+
+# project imports
+from bushido.exceptions import ValidationError
+from bushido.schema.scale import KeikoSpec
+from bushido.utils.parsing import parse_start_end_time_string
+from bushido.data.categories.gym import create_keiko_orm
+from bushido.data.categories import AbsCategory, AbsProcessor, AbsKeikoTable
+
+
+class Category(AbsCategory):
+    def __init__(self, engine):
+        super().__init__(engine)
+        self.keiko = KeikoTable
+
+
+class Processor(AbsProcessor):
+    def __init__(self, engine):
+        super().__init__(engine)
+
+    def process_keiko(self, unit, words):
+        try:
+            weight = float(words[0])
+        except (IndexError, ValueError):
+            raise ValueError('wrong format')
+
+        try:
+            fat = float(words[1])
+        except IndexError:
+            fat = None
+
+        try:
+            water = float(words[2])
+        except IndexError:
+            water = None
+
+        try:
+            muscles = float(words[3])
+        except IndexError:
+            muscles = None
+
+        with Session(self.engine) as session:
+            session.add(unit)
+            session.commit()
+            keiko = KeikoTable(weight=weight,
+                               fat=fat,
+                               water=water,
+                               muscles=muscles,
+                               fk_unit=unit.key)
+            session.add(keiko)
+            session.commit()
+
+
+class KeikoTable(AbsKeikoTable):
+    __tablename__ = 'scale'
+
+    weight: Mapped[float] = mapped_column()
+    fat: Mapped[float] = mapped_column()
+    water: Mapped[float] = mapped_column()
+    muscles: Mapped[float] = mapped_column()
