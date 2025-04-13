9e56a002f5b3d410e12b1463a6fca274b75d8f34
Noe Javet <np.javet@gmail.com>
2025-04-13 17:04:57 +0200

    scale data

--- bushido/data/categories/scale.py
+++ bushido/data/categories/scale.py
@@ -1,56 +1,18 @@
-from sqlalchemy.orm import mapped_column, Mapped, Session
+from typing import Optional
+from sqlalchemy.orm import mapped_column, Mapped
 
 # project imports
-from bushido.data.categories import AbsCategory, AbsProcessor, AbsKeikoTable
+from bushido.data.base_tables import AbsKeikoTable
 
 
-class Category(AbsCategory):
-    def __init__(self, engine):
-        super().__init__(engine)
-        self.keiko = KeikoTable
-
-
-class Processor(AbsProcessor):
-    def __init__(self, engine):
-        super().__init__(engine)
-
-    def process_keiko(self, unit, words):
-        try:
-            weight = float(words[0])
-        except (IndexError, ValueError):
-            raise ValueError('wrong format')
-
-        try:
-            fat = float(words[1])
-        except IndexError:
-            fat = None
-
-        try:
-            water = float(words[2])
-        except IndexError:
-            water = None
-
-        try:
-            muscles = float(words[3])
-        except IndexError:
-            muscles = None
-
-        with Session(self.engine) as session:
-            session.add(unit)
-            session.commit()
-            keiko = KeikoTable(weight=weight,
-                               fat=fat,
-                               water=water,
-                               muscles=muscles,
-                               fk_unit=unit.key)
-            session.add(keiko)
-            session.commit()
+def create_keiko_orm(keiko_spec):
+    keiko = KeikoTable(weight=keiko_spec.weight,
+                       belly=keiko_spec.belly)
+    return keiko
 
 
 class KeikoTable(AbsKeikoTable):
     __tablename__ = 'scale'
 
     weight: Mapped[float] = mapped_column()
-    fat: Mapped[float] = mapped_column()
-    water: Mapped[float] = mapped_column()
-    muscles: Mapped[float] = mapped_column()
+    belly: Mapped[Optional[float]] = mapped_column()
