9a07ec64dcc3271d566736444763bdfd732d0432
Noe Javet <np.javet@gmail.com>
2025-04-13 21:03:57 +0200

    repository pattern

---
+++ bushido/data/category.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from bushido.data.base_tables import MDEmojiTable, MDCategoryTable
+
+
+class CategoryRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_category_for_unit(self, unit_name):
+        stmt = (select(MDCategoryTable.name)
+                .join(MDEmojiTable)
+                .where(MDEmojiTable.unit_name == unit_name))
+        return self.session.execute(stmt).scalar()
