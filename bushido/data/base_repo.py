2870d8b8b96c57a29259758f566d74bc81a21f07
Noe Javet <np.javet@gmail.com>
2025-04-14 11:52:47 +0200

    repo inheritance

--- bushido/data/base_repo.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-
-
-class BaseRepository:
-    def __init__(self, session: Session):
-        self.session = session
--- bushido/data/unit.py
+++ bushido/data/unit.py
@@ -1,64 +1,39 @@
-from sqlalchemy import select, or_
+from sqlalchemy import select
 from sqlalchemy.orm import Session
-# project imports
-from bushido.data.models import UnitTable, MDEmojiTable, MDCategoryTable
+from bushido.data.base_models import MDEmojiModel, UnitModel
 
 
 class UnitRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_all(self):
-        stmt = select(MDEmojiTable.emoji, MDEmojiTable.unit_name)
-        return self.session.execute(stmt).all()
-
-    def get_emoji_for_unit(self, unit_name: str):
-        stmt = select(MDEmojiTable.emoji).where(MDEmojiTable.unit_name == unit_name)
-        return self.session.scalar(stmt)
-
-    def get_emoji_key_by_unit(self, unit_name: str):
-        stmt = (select(MDEmojiTable.key)
-                .where(MDEmojiTable.unit_name == unit_name))
-        emoji_key = self.session.scalar(stmt)
-        return emoji_key
-
     def get_units(self, unit_name=None, start_t=None, end_t=None):
-        stmt = (select(MDEmojiTable.emoji,
-                       UnitTable.timestamp,
-                       UnitTable.payload,
-                       UnitTable.comment)
-                .join(UnitTable, MDEmojiTable.key == UnitTable.fk_emoji))
+        stmt = (select(MDEmojiModel.emoji,
+                       UnitModel.timestamp,
+                       UnitModel.payload,
+                       UnitModel.comment)
+                .join(UnitModel, MDEmojiModel.key == UnitModel.fk_emoji))
 
         if unit_name:
-            stmt = stmt.where(MDEmojiTable.unit_name == unit_name)
+            stmt = stmt.where(MDEmojiModel.unit_name == unit_name)
         if start_t:
-            stmt = stmt.where(start_t.timestamp() <= UnitTable.timestamp)
+            stmt = stmt.where(start_t.timestamp() <= UnitModel.timestamp)
         if end_t:
-            stmt = stmt.where(UnitTable.timestamp <= end_t.timestamp())
+            stmt = stmt.where(UnitModel.timestamp <= end_t.timestamp())
 
         return self.session.execute(stmt).all()
 
-    def get_category_for_unit(self, unit_name):
-        stmt = (select(MDCategoryTable.name)
-                .join(MDEmojiTable)
-                .where(MDEmojiTable.unit_name == unit_name))
-        return self.session.execute(stmt).scalar()
-
-    def get_unit_name_for_emoji(self, emoji: str):
-        stmt = (select(MDEmojiTable.unit_name)
-                .where(or_(MDEmojiTable.emoji == emoji,
-                           MDEmojiTable.emoticon == emoji)))
-        return self.session.scalar(stmt)
-
-    def save_unit_and_keiko(self, unit, keiko):
+    def save_unit(self, unit):
         self.session.add(unit)
         self.session.commit()
+        return unit.key
 
+    def save_keiko(self, unit_key, keiko):
         if isinstance(keiko, list):
             for k in keiko:
-                k.fk_unit = unit.key
+                k.fk_unit = unit_key
             self.session.add_all(keiko)
         else:
-            keiko.fk_unit = unit.key
+            keiko.fk_unit = unit_key
             self.session.add(keiko)
         self.session.commit()
