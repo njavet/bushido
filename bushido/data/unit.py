6c3926cd9dfdbc06fb0692622e4c5ac475d73e18
Noe Javet <np.javet@gmail.com>
2025-04-14 12:22:16 +0200

    log service

--- bushido/data/unit.py
+++ bushido/data/unit.py
@@ -1,11 +1,37 @@
-from sqlalchemy import select
+from sqlalchemy import select, or_
 from sqlalchemy.orm import Session
-from bushido.data.base_models import MDEmojiModel, UnitModel
+from bushido.data.base_models import MDEmojiModel, MDCategoryModel, UnitModel
 
 
 class UnitRepository:
     def __init__(self, session: Session):
         self.session = session
+
+    def get_all_emojis(self):
+        stmt = select(MDEmojiModel.emoji, MDEmojiModel.unit_name)
+        return self.session.execute(stmt).all()
+
+    def get_emoji_for_unit(self, unit_name: str):
+        stmt = select(MDEmojiModel.emoji).where(MDEmojiModel.unit_name == unit_name)
+        return self.session.scalar(stmt)
+
+    def get_emoji_key_by_unit(self, unit_name: str):
+        stmt = (select(MDEmojiModel.key)
+                .where(MDEmojiModel.unit_name == unit_name))
+        emoji_key = self.session.scalar(stmt)
+        return emoji_key
+
+    def get_unit_name_for_emoji(self, emoji: str):
+        stmt = (select(MDEmojiModel.unit_name)
+                .where(or_(MDEmojiModel.emoji == emoji,
+                           MDEmojiModel.emoticon == emoji)))
+        return self.session.scalar(stmt)
+
+    def get_category_for_unit(self, unit_name):
+        stmt = (select(MDCategoryModel.name)
+                .join(MDEmojiModel)
+                .where(MDEmojiModel.unit_name == unit_name))
+        return self.session.execute(stmt).scalar()
 
     def get_units(self, unit_name=None, start_t=None, end_t=None):
         stmt = (select(MDEmojiModel.emoji,
--- bushido/service/unit.py
+++ bushido/service/unit.py
@@ -1,4 +1,6 @@
-from bushido.data.models import UnitTable
+import datetime
+from bushido.conf import LOCAL_TIME_ZONE
+from bushido.data.base_models import UnitModel
 from bushido.data.unit import UnitRepository
 
 
@@ -23,13 +25,25 @@ class UnitService:
     def get_category_for_unit(self, unit_name):
         return self.unit_repo.get_category_for_unit(unit_name)
 
+    def process_unit(self, spec):
+        unit = self.create_unit(spec)
+        unit_key = self.unit_repo.save_unit(unit)
+        keiko = self.create_keiko(spec.words)
+        self.unit_repo.save_keiko(unit_key, keiko)
+
+    def set_timestamp(self):
+        now = datetime.datetime.now().replace(tzinfo=LOCAL_TIME_ZONE)
+        timestamp = int(now.timestamp())
+        return timestamp
+
     def create_unit(self, spec):
         emoji_key = self.unit_repo.get_emoji_key_by_unit(spec.unit_name)
-        unit = UnitTable(timestamp=spec.timestamp,
+        timestamp = self.set_timestamp()
+        unit = UnitModel(timestamp=timestamp,
                          payload=' '.join(spec.words),
                          comment=spec.comment,
                          fk_emoji=emoji_key)
         return unit
 
-    def upload_units(self, unit_spec):
-        pass
+    def create_keiko(self, words):
+        raise NotImplementedError
