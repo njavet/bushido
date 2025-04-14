bff8a590c4c54ae54352b877ca5aa7461715bb2e
Noe Javet <np.javet@gmail.com>
2025-04-14 15:05:56 +0200

    base service

--- bushido/service/base_service.py
+++ bushido/service/base_service.py
@@ -1,7 +1,7 @@
 from bushido.data.base_repo import BaseRepository
 
 
-class EmojiService:
+class BaseService:
     def __init__(self, base_repo: BaseRepository):
         self.repo = base_repo
 
@@ -12,3 +12,12 @@ class EmojiService:
     def get_all_emojis(self):
         rows = self.repo.get_all_emojis()
         return [dict(key=r.unit_name, value=r.emoji) for r in rows]
+
+    def emoji_for_unit(self, unit_name: str):
+        return self.repo.get_emoji_for_unit(unit_name)
+
+    def unit_name_for_emoji(self, emoji: str):
+        return self.repo.get_unit_name_for_emoji(emoji)
+
+    def get_category_for_unit(self, unit_name):
+        return self.repo.get_category_for_unit(unit_name)
