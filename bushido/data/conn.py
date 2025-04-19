c9bdd789a20d54b482338e0ea6076ebc30dc4592
Noe Javet <np.javet@gmail.com>
2025-04-19 13:26:10 +0200

    session factory class

--- bushido/data/conn.py
+++ bushido/data/conn.py
@@ -4,21 +4,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from bushido.conf import DB_URL
 
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 
-def get_session():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class SessionFactory:
+    def __init__(self, db_url=DB_URL):
+        self._engine = create_engine(db_url)
+        self._sessionmaker = sessionmaker(bind=self._engine,
+                                          expire_on_commit=False)
 
+    def get_session(self):
+        db = self._sessionmaker()
+        try:
+            yield db
+        finally:
+            db.close()
 
-@contextmanager
-def get_session_context():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    @contextmanager
+    def get_session_context(self):
+        db = self._sessionmaker()
+        try:
+            yield db
+        finally:
+            db.close()
+
