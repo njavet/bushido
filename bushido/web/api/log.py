a06deefcaaa06d00e8a3c617cfb02a3f6dfe1a2a
Noe Javet <np.javet@gmail.com>
2025-04-19 13:49:19 +0200

    depends vs app state

--- bushido/web/api/log.py
+++ bushido/web/api/log.py
@@ -12,19 +12,19 @@ router = APIRouter()
 
 @router.post('/api/log_unit')
 async def log_unit(request: Request):
-    session = request.app.state.sf.get_session()
-    base_service = BaseService.from_session(session)
-    data = await request.json()
-    try:
-        emoji, words, comment = preprocess_input(data['text'])
-    except ValidationError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    with request.app.state.sf.get_session_context() as session:
+        base_service = BaseService.from_session(session)
+        data = await request.json()
+        try:
+            emoji, words, comment = preprocess_input(data['text'])
+        except ValidationError as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
-    unit_name = base_service.unit_name_for_emoji(emoji)
-    category = base_service.get_category_for_unit(unit_name)
-    log_service = load_log_service(category).from_session(session)
-    log_service.process_unit(unit_name, words, comment)
-    return {'res': 'ok'}
+        unit_name = base_service.unit_name_for_emoji(emoji)
+        category = base_service.get_category_for_unit(unit_name)
+        log_service = load_log_service(category).from_session(session)
+        log_service.process_unit(unit_name, words, comment)
+        return {'res': 'ok'}
 
 
 def load_log_service(category: str, package: str = KEIKO_PROCESSORS):
