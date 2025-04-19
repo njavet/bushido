031a25b7e87436f88b4c5c8136f9aa64d2254d3a
Noe Javet <np.javet@gmail.com>
2025-04-19 12:18:55 +0200

    add js router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/Home.vue';
-import About from '../components/About.vue';
-import Lab0 from '../components/Lab0.vue';
-import TychoStation from "../components/TychoStation.vue";
-import Bushido from '../components/Bushido.vue';
+import Home from '../App.vue';
+import Units from '../components/Units.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/lab0', component: Lab0 },
-  { path: '/tycho-station', component: TychoStation },
-  { path: '/bushido', component: Bushido },
+  { path: '/units', component: Units },
 ];
 
 const router = createRouter({
