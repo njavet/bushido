30f871762ab5c0570ad8658223786cf99e0e4b54
Noe Javet <np.javet@gmail.com>
2025-04-19 13:36:53 +0200

    adjust apis

--- bushido/web/api/base.py
+++ bushido/web/api/base.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Request
 from bushido.service.base import BaseService
-from bushido.data.conn import get_session
 
 
 router = APIRouter()
 
 
 @router.get('/api/emojis')
-async def get_emojis(session: Session = Depends(get_session)):
+async def get_emojis(request: Request):
+    session = request.app.state.sf.get_session()
     service = BaseService.from_session(session)
     return service.get_all_emojis()
--- bushido/web/api/unit.py
+++ bushido/web/api/unit.py
@@ -1,21 +1,17 @@
 from collections import defaultdict
-from fastapi import Request, APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import Request, APIRouter
 from bushido.utils.dt_functions import get_bushido_date_from_timestamp
-from bushido.data.conn import get_session
 from bushido.service.unit import UnitService
 
 
 router = APIRouter()
 
 
-
 @router.get('/api/get_units')
-async def log_unit(request: Request,
-                   session: Session = Depends(get_session)):
+async def log_unit(request: Request):
+    session = request.app.state.sf.get_session()
     base_service = UnitService.from_session(session)
     units = base_service.get_units()
-
     dix = defaultdict(list)
     for unit in units:
         dt = get_bushido_date_from_timestamp(unit.timestamp)
