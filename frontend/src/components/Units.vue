c207dc5e54a17c83e24e89b75136003d43f906d8
Noe Javet <np.javet@gmail.com>
2025-04-29 18:02:22 +0200

    units frontend

--- frontend/src/components/Units.vue
+++ frontend/src/components/Units.vue
@@ -1,102 +1,46 @@
 <template>
-  <div class="dashboard">
-    <div class="right-panel">
-      <div class="history" ref="historyContainer">
-        <h2>Unit History</h2>
-          <section v-for="(entries, date) in unitsByDay" :key="date" class="panel">
-          <h3><span class="text-cyan-200">{{ date }}</span></h3>
-          <ul>
-            <li v-for="(entry, i) in entries" :key="i">
-              <span class="text-cyan-100">{{ entry[0] }} {{ entry[1] }} {{ entry[2] }}</span>
-            </li>
-          </ul>
-          </section>
-    </div>
-  </div>
+  <div class="chat">
+    <ChatContainer
+      :messages="messages"
+    />
+    <ChatInput
+      @send="handleUserMessage"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref } from 'vue'
+import ChatContainer from "./ChatContainer.vue";
+import ChatInput from './ChatInput.vue'
+import { sendMessage } from '../js/chatUtils'
 
-const inputValue = ref("")
-const terminalInput = ref(null)
-const unitsByDay = ref({})
-const historyContainer = ref(null)
+const messages = ref([])
 
-async function fetchUnits() {
-  const res = await fetch('/api/get_units')
-  const data = await res.json()
-  console.log(data)
-  unitsByDay.value = data
-}
+const props = defineProps({
+  base_url: String,
+  lm_name: String,
+  system_message: String
+})
 
+function handleUserMessage(text) {
+  if (!text) return
+  messages.value.push({ role: 'User', text: text })
 
-
-async function handleEnter() {
-  const res = await fetch('/api/log_unit', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ text: inputValue.value })
+  sendMessage({
+    text,
+    props,
+  }).then(response => {
+    messages.value.push({ role: 'Bot', text: response })
   })
-  const data = await res.json()
-  unitsByDay.value.push(inputValue.value)
-  inputValue.value = ""
 }
-
-
-watch(unitsByDay, scrollToBottom)
-
 </script>
-
-<style>
-.dashboard {
-  display: flex;
-  height: 100vh;
-  font-family: sans-serif;
-}
-
-.left-panel,
-.right-panel {
-  flex: 1;
-  padding: 20px;
-  box-sizing: border-box;
-  overflow-y: auto;
-}
-
-.left-panel {
-  background-color: #222;
-  color: #fff;
-}
-
-.right-panel {
-  background-color: #111;
-  color: #eee;
+<style scoped>
+.chat {
   display: flex;
   flex-direction: column;
-}
-
-.graphic-box {
-  background-color: #333;
-  padding: 20px;
-  border-radius: 8px;
-  margin-bottom: 20px;
-}
-
-.history {
-  flex: 1;
-  overflow-y: auto;
-  margin-bottom: 10px;
-}
-
-.input-area input {
+  height: 100vh;
   width: 100%;
-  padding: 10px;
-  border-radius: 6px;
-  border: none;
-  font-size: 1em;
-  background-color: #222;
-  color: white;
-  outline: none;
+  font-family: monospace;
 }
 </style>
