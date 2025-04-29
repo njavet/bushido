d68f592563ddcc17a643839ed02b80d17b911618
Noe Javet <np.javet@gmail.com>
2025-04-29 17:27:31 +0200

    sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar">
+    <h3>Navigation</h3>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.sidebar {
+  width: 220px;
+  flex-shrink: 0;
+  background-color: #1f1f1f;
+  color: white;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-right: 1px solid #444;
+  text-align: left;
+}
+</style>
