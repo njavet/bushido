27f038699c05f747909ca55710e8f34d82ccd4ae
Noe Javet <np.javet@gmail.com>
2025-04-30 14:17:55 +0200

    gym

---
+++ bushido/data/categories/gym.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from datetime import time
+from sqlalchemy.orm import Mapped, mapped_column, Session
+
+# project imports
+from bushido.data.base_models import AbsKeikoModel
+from bushido.data.base_repo import BaseRepository
+
+
+class GymModel(AbsKeikoModel):
+    __tablename__ = 'gym'
+
+    start_t: Mapped[time] = mapped_column()
+    end_t: Mapped[time] = mapped_column()
+    gym: Mapped[Optional[str]] = mapped_column()
+
+
+class GymRepository(BaseRepository):
+    def __init__(self, session: Session):
+        super().__init__(session)
