2f8c5921a7b92d57d3ade0555431db6b2c47f645
Noe Javet <np.javet@gmail.com>
2025-04-30 14:43:26 +0200

    cardio

---
+++ bushido/data/categories/cardio.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from datetime import time
+from sqlalchemy.orm import Mapped, mapped_column, Session
+
+# project imports
+from bushido.data.base_models import AbsKeikoModel
+from bushido.data.base_repo import BaseRepository
+
+
+class CardioModel(AbsKeikoModel):
+    __tablename__ = 'cardio'
+
+    start_t: Mapped[time] = mapped_column()
+    seconds: Mapped[float] = mapped_column()
+    gym: Mapped[str] = mapped_column()
+    distance: Mapped[Optional[float]] = mapped_column()
+
+
+class CardioRepository(BaseRepository):
+    def __init__(self, session: Session):
+        super().__init__(session)
