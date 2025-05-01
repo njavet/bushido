0cec9e611625071def2572551c1d4bf9883f2596
Noe Javet <np.javet@gmail.com>
2025-05-01 18:43:51 +0200

    include router

--- bushido/web/categories/lifting.py
+++ bushido/web/categories/lifting.py
@@ -1,16 +1,16 @@
 from sqlalchemy.orm import Session
-from fastapi import Request, APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 
 # project imports
 from bushido.web.deps import get_session
-from bushido.service.categories.wimhof import UnitService
+from bushido.service.categories.lifting import UnitService
 
 
 router = APIRouter()
 
 
-@router.get('/api/get-wimhof-units')
-async def get_wimhof_units(session: Session = Depends(get_session)):
+@router.get('/api/get-lifting-units')
+async def get_lifting_units(session: Session = Depends(get_session)):
     service = UnitService.from_session(session)
     return service.get_units()
 
