02c92b85cd4e8b07abef4ff25fd3921f4b8daf90
Noe Javet <np.javet@gmail.com>
2025-05-01 20:08:27 +0200

    tabs

--- frontend/src/components/Lifting.vue
+++ frontend/src/components/Lifting.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="container">
-    <vue-good-table
-      :columns="columns"
-      :rows="liftingUnits"
-      :search-options="{ enabled: true }"
-      :pagination-options="{ enabled: false }"
-    />
+    <div class="tab-bar">
+      <button
+        v-for="tab in tabs"
+        :key="tab.key"
+        :class="{ active: currentTab === tab.key}"
+        @click="currentTab = tab.key">
+        {{ tab.label }}
+      </button>
+      <vue-good-table
+          :columns="columns"
+          :rows="liftingUnits"
+          :search-options="{ enabled: true }"
+          :pagination-options="{ enabled: false }"
+      />
+    </div>
   </div>
 </template>
 
@@ -14,8 +23,13 @@ import { ref, onMounted } from 'vue'
 import { VueGoodTable } from 'vue-good-table-next'
 import 'vue-good-table-next/dist/vue-good-table-next.css'
 
+const props = defineProps(['emojis'])
+const tabs = props.emojis.map(([key, value]) => ({
+  key,
+  label: key,
+}))
+const currentTab = ref(tabs.length ? tabs[0].key : null)
 const liftingUnits = ref([])
-const tabs = []
 
 const columns = [
   { label: 'Date', field: 'date', sortable: true },
@@ -26,10 +40,14 @@ const columns = [
 ]
 
 onMounted(async () => {
-  const tabRes = await fetch('/api/lifting/get-units')
   const res = await fetch('/api/get-lifting-units')
   liftingUnits.value = await res.json()
 })
+
+function filterUnits(unitName) {
+
+}
+
 </script>
 
 <style scoped>
@@ -38,5 +56,23 @@ onMounted(async () => {
   background: #1f1f1f;
   color: white;
   border-radius: 8px;
+}
+.tab-bar {
+  display: flex;
+  gap: 1rem;
+  margin: 1rem;
+}
+.tab-bar button {
+  background: #444;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background 0.2s ease;
+}
+.tab-bar button.active {
+  background: cyan;
+  color: black;
 }
 </style>
