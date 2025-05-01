63d0b6e5d572297ca0c9debce11c9c4c40550571
Noe Javet <np.javet@gmail.com>
2025-05-01 18:43:51 +0200

    include router

--- bushido/web/__init__.py
+++ bushido/web/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 # project imports
 from .endpoints import router as base_router
+from .categories.lifting import router as lifting_router
 from .categories.wimhof import router as wimhof_router
 
 router = APIRouter()
 router.include_router(base_router)
+router.include_router(lifting_router)
 router.include_router(wimhof_router)
