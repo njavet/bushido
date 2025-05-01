572d04a12bd7e3720af43a42cbbbfe753ebdddfb
Noe Javet <np.javet@gmail.com>
2025-05-01 20:08:27 +0200

    tabs

--- bushido/web/categories/lifting.py
+++ bushido/web/categories/lifting.py
@@ -8,11 +8,6 @@ from bushido.service.categories.lifting import UnitService
 
 router = APIRouter()
 
-@router.get('/api/lifting/get-units')
-async def get_units(session: Session = Depends(get_session)):
-    service = UnitService.from_session(session)
-    return service.get_units()
-
 
 @router.get('/api/get-lifting-units')
 async def get_lifting_units(session: Session = Depends(get_session)):
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,17 +8,18 @@
     <div class="main-content">
       <router-view
           :key="selectedOption"
-          :emojis="emojis"/>
+          :emojis="filterEmojis(selectedOption)"/>
     </div>
   </div>
 </template>
 
 <script setup>
-import {ref, onMounted} from "vue";
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import Sidebar from "./components/Sidebar.vue"
 
 const router = useRouter()
+const md = ref([])
 const emojis = ref([])
 const navOptions = ref([
     {key: 'units', value: 'Units'}
@@ -27,20 +28,31 @@ const selectedOption = ref('units')
 
 onMounted(async() => {
   const res = await fetch('/api/get-master-data')
-  const md = await res.json()
-  const newOptions = Object.keys(md.categories)
+  md.value = await res.json()
+  const newOptions = Object.keys(md.value.categories)
       .map(cat => ({
         key: cat,
         value: cat.charAt(0).toUpperCase() + cat.slice(1)
   }))
   navOptions.value.push(...newOptions)
-  emojis.value = md
-      .flatMap(cat => cat.emojis)
-      .map(item => ({
-        key: item.unit_name,
-        value: item.emoji
-      }))
+  emojis.value = Object.entries(md.value.categories)
+      .flatMap(([_, pairs]) =>
+          pairs.map(([emoji, unit_name]) => ({
+            key: unit_name,
+            value: emoji
+      })))
 })
+
+function filterEmojis(category) {
+  if (category === 'units') {
+    return emojis.value
+  } else {
+    return md.value.categories[category].map(([emoji, unit_name]) => ({
+      key: unit_name,
+      value: emoji
+    }))
+  }
+}
 
 function handleSelect(key) {
   selectedOption.value = key
