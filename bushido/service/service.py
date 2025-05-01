3d45dc8e4da1276294462f39d42f1f38e470301e
Noe Javet <np.javet@gmail.com>
2025-05-01 14:02:02 +0200

    service interface, maybe too many layers again

--- bushido/service/service.py
+++ bushido/service/service.py
@@ -1,5 +1,24 @@
+# project imports
+from bushido.service.unit import BaseUnitService
+from bushido.service.log import LogService
 
+
+def get_categories(session):
+    service = BaseUnitService.from_session(session)
+    return service.get_all_categories()
+
+
+def get_emojis(session):
+    service = BaseUnitService.from_session(session)
+    return service.get_all_emojis()
+
+
+def get_units(session):
+    service = BaseUnitService.from_session(session)
+    return service.get_all_units()
 
 
 def log_unit(text, session):
-    pass
+    service = LogService.from_session(session)
+    unit_log_res = service.log_unit(text)
+    return unit_log_res
--- bushido/web/endpoints.py
+++ bushido/web/endpoints.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
-from fastapi import Request, APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends
 
 # project imports
 from bushido.exceptions import ValidationError
 from bushido.schema.req import UnitLogRequest
-from bushido.service.unit import BaseUnitService
+from bushido.service import service
 from bushido.web.deps import get_session
 
 
@@ -12,29 +12,25 @@ router = APIRouter()
 
 
 @router.get('/api/get-categories')
-async def get_emojis(session: Session = Depends(get_session)):
-    service = BaseUnitService.from_session(session)
-    return service.get_all_categories()
+async def get_categories_service(session: Session = Depends(get_session)):
+    return service.get_categories(session)
 
 
 @router.get('/api/get-emojis')
-async def get_emojis(session: Session = Depends(get_session)):
-    service = BaseUnitService.from_session(session)
-    return service.get_all_emojis()
+async def get_emojis_service(session: Session = Depends(get_session)):
+    return service.get_emojis(session)
 
 
 @router.get('/api/get-units')
-async def get_units(session: Session = Depends(get_session)):
-    service = BaseUnitService.from_session(session)
-    return service.get_all_units()
+async def get_units_service(session: Session = Depends(get_session)):
+    return service.get_units(session)
 
 
 @router.post('/api/log-unit')
 async def log_unit_service(unit_log_request: UnitLogRequest,
                            session: Session = Depends(get_session)):
     try:
-        service = BaseUnitService.from_session(session)
-        unit_log_res = service.log_unit(unit_log_request.text)
+        unit_log_res = service.log_unit(unit_log_request.text, session)
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return unit_log_res
