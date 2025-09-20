fb29768898c56bf31fc2ce90736eef803c261f8b
Noe Javet <np.javet@gmail.com>
2025-09-20 12:42:51 +0200

    delete more old code

--- bushido/core/conf.py
+++ bushido/core/conf.py
@@ -1,5 +1,4 @@
 from zoneinfo import ZoneInfo
-from pathlib import Path
 
 DEFAULT_PORT = 8080
 
@@ -7,8 +6,4 @@ LOCAL_TIME_ZONE = ZoneInfo('Europe/Zurich')
 
 DAY_START_HOUR = 4
 
-# directories
-MASTER_DATA_DIR = Path('bushido', 'static', 'master_data')
-
 DB_URL = 'sqlite:///bushido.db'
-
