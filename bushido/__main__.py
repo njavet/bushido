ca7597b729901324e92642c0f85e70d1149315af
Noe Javet <np.javet@gmail.com>
2025-09-20 13:14:45 +0200

    __main__

--- bushido/__main__.py
+++ bushido/__main__.py
@@ -1,13 +1,38 @@
+import sys
+import argparse
 import uvicorn
 
+# project imports
 from bushido.core.conf import DEFAULT_PORT
+from bushido import __version__
 
 
-def run_app():
-    uvicorn.run('bushido.main:create_app',
-                port=DEFAULT_PORT,
-                reload=True,
-                factory=True,
-                log_level='debug')
+def create_argparser():
+    parser = argparse.ArgumentParser(description='bushido server')
+    parser.add_argument('--version', action='store_true', help='show version')
+    parser.add_argument('--devel', action='store_true', help='run development server')
+    return parser
 
-run_app()
+
+def main():
+    parser = create_argparser()
+    args = parser.parse_args()
+    if args.version:
+        print(f'bushido v{__version__}')
+        sys.exit(0)
+
+    if args.devel:
+        uvicorn.run('bushido.main:create_app',
+                    port=DEFAULT_PORT,
+                    reload=True,
+                    factory=True,
+                    log_level='debug')
+    else:
+        uvicorn.run('bushido.main:create_app',
+                    port=DEFAULT_PORT,
+                    factory=True,
+                    log_level='info')
+
+
+if __name__ == '__main__':
+    main()
