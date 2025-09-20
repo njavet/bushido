748d78a2137c466a0fc6daf30e53e977a48042a6
Noe Javet <np.javet@gmail.com>
2025-09-20 13:12:02 +0200

    main

--- bushido/main.py
+++ bushido/main.py
@@ -1,12 +1,33 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from rich.logging import RichHandler
 from starlette.middleware.cors import CORSMiddleware
 
 # project imports
+from bushido.core.app_context import app_context
+from bushido.db.conn import SessionFactory
 from bushido.web import router
 
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[RichHandler(rich_tracebacks=True, show_time=False)])
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    dbs = {
+        'sqlite': SessionFactory('sqlite')
+    }
+    app_context.dbs = dbs
+    yield
+    app_context.dbs = None
+
+
 def create_app():
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     app.add_middleware(CORSMiddleware,
                        allow_origins=['http://localhost:5173'],
