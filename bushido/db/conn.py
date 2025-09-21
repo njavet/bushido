adca1f8e23f42bff849d858e5d44ef5ec32c51e5
Noe Javet <np.javet@gmail.com>
2025-09-21 15:36:56 +0200

    fk for lifting

--- bushido/db/conn.py
+++ bushido/db/conn.py
@@ -1,14 +1,16 @@
+from collections.abc import Generator, Iterator
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker, Session
 
 from bushido.core.conf import DB_URL
 from bushido.db.model.base import Base
 
 
 class SessionFactory:
-    def __init__(self, db_url=DB_URL):
+    def __init__(self, db_url: str = DB_URL) -> None:
         self._frozen = False
         self.db_url = db_url
         self._engine = create_engine(db_url)
@@ -18,21 +20,21 @@ class SessionFactory:
         self._frozen = True
 
     @property
-    def engine(self):
+    def engine(self) -> Engine:
         return self._engine
 
     @property
-    def db_url(self):
+    def db_url(self) -> str:
         return self._db_url
 
     @db_url.setter
-    def db_url(self, db_url):
+    def db_url(self, db_url: str) -> None:
         if getattr(self, '_frozen', True):
             raise AttributeError('db_url is read only after init...')
         else:
             self._db_url = db_url
 
-    def get_session(self):
+    def get_session(self) -> Generator[Session, None, None]:
         db = self._sessionmaker()
         try:
             yield db
@@ -40,14 +42,14 @@ class SessionFactory:
             db.close()
 
     @contextmanager
-    def get_session_context(self):
+    def get_session_context(self) -> Iterator[Session]:
         db = self._sessionmaker()
         try:
             yield db
         finally:
             db.close()
 
-    def _setup_engine(self):
+    def _setup_engine(self) -> Engine:
         engine = create_engine(self.db_url)
         Base.metadata.create_all(bind=engine)
         return engine
--- bushido/db/model/lifting.py
+++ bushido/db/model/lifting.py
@@ -1,7 +1,18 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # project imports
 from bushido.db.model.base import Base
+
+
+class Exercise(Base):
+    __tablename__ = 'exercise'
+
+    name: Mapped[str] = mapped_column()
+    sets = relationship(
+        back_populates='session',
+        cascade='all, delete-orphan',
+    )
 
 
 class LiftingSet(Base):
@@ -10,14 +21,8 @@ class LiftingSet(Base):
     weight: Mapped[float] = mapped_column()
     reps: Mapped[float] = mapped_column()
     rest: Mapped[float] = mapped_column(default=0)
+    fk_exercise: Mapped[int] = mapped_column(ForeignKey(Exercise.id))
 
-    exercise = relationship('Exercise', back_populates='lifting_sets')
-
-
-class Exercise(Base):
-    __tablename__ = 'exercise'
-
-    name: Mapped[str] = mapped_column()
-    sets: list[LiftingSet] = mapped_column()
-
-    lifting_sets = relationship('LiftingSet', back_populates='exercise')
+    session = relationship(
+        back_populates='sets',
+    )
