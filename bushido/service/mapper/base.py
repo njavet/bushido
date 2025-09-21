2897c910438ba57f6a577f9388c181c28cc800b4
Noe Javet <np.javet@gmail.com>
2025-09-21 17:10:31 +0200

    lifting mapper

--- bushido/service/mapper/base.py
+++ bushido/service/mapper/base.py
@@ -1,11 +1,11 @@
 from typing import Protocol
 
 # project imports
-from bushido.core.types import ORM_T, UNIT_T
+from bushido.core.types import ORM_T, ORM_ST, UNIT_T
 from bushido.domain.base import ParsedUnit
 
 
-class UnitMapper(Protocol):
-    def to_orm(self, parsed_unit: ParsedUnit[UNIT_T]) -> list[ORM_T]: ...
+class UnitMapper(Protocol[UNIT_T, ORM_T, ORM_ST]):
+    def to_orm(self, parsed_unit: ParsedUnit[UNIT_T]) -> tuple[ORM_T, list[ORM_ST]]: ...
 
-    def from_orm(self, orm_lst: list[ORM_T]) -> ParsedUnit[UNIT_T]: ...
+    def from_orm(self, orms: tuple[ORM_T, list[ORM_ST]]) -> ParsedUnit[UNIT_T]: ...
