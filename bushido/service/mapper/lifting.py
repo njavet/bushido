349e640cab9644273daa221d82665ec4eb1c71dc
Noe Javet <np.javet@gmail.com>
2025-09-21 17:10:31 +0200

    lifting mapper

--- bushido/service/mapper/lifting.py
+++ bushido/service/mapper/lifting.py
@@ -1,11 +1,30 @@
 # project imports
 from bushido.domain.base import ParsedUnit
 from bushido.db import Exercise, LiftingSet
+from bushido.domain.lifting import ExerciseSpec, SetSpec
 from bushido.service.mapper.base import UnitMapper
 
 
-class LiftingMapper(UnitMapper):
-    def to_orm(self, parsed_unit: ParsedUnit
-    ) -> list[ORM_T]: ...
+class LiftingMapper(UnitMapper[ExerciseSpec, Exercise, LiftingSet]):
+    def to_orm(self, parsed_unit: ParsedUnit[ExerciseSpec]
+    ) -> tuple[Exercise, list[LiftingSet]]:
+        unit = Exercise(name=parsed_unit.unit_name,
+                        comment=parsed_unit.comment)
+        lst = []
+        for s in parsed_unit.data.sets:
+            ls = LiftingSet(weight=s.weight,
+                            reps=s.reps,
+                            rest=s.rest)
+            lst.append(ls)
+        return unit, lst
 
-    def from_orm(self, orm_lst: list[ORM_T]) -> ParsedUnit: ...
+    def from_orm(self, orms: tuple[Exercise, list[LiftingSet]]) -> ParsedUnit[ExerciseSpec]:
+        unit, sets = orms
+        lst = []
+        for s in sets:
+            sp = SetSpec(weight=s.weight, reps=s.reps, rest=s.rest)
+            lst.append(sp)
+        pu = ParsedUnit(unit_name=unit.name,
+                        data=ExerciseSpec(sets=lst),
+                        comment=unit.comment)
+        return pu
