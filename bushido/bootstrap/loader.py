76a955b1e56de977c0645c18aa221d42a5e2de32
Noe Javet <np.javet@gmail.com>
2025-09-21 19:21:51 +0200

    cast for clean mypy

--- bushido/bootstrap/loader.py
+++ bushido/bootstrap/loader.py
@@ -1,14 +1,20 @@
+from types import MappingProxyType
+from typing import Any, Mapping
+
 # project imports
 from bushido.core.unit import LiftingUnitName
+from bushido.service.parser.base import UnitParser
+from bushido.service.mapper.base import UnitMapper
 from bushido.service.parser.lifting import LiftingParser
 from bushido.service.mapper.lifting import LiftingMapper
 
 
-def load_parsers() -> dict:
-    lifting_parsers = {u.name: LiftingParser for u in LiftingUnitName}
-    return lifting_parsers
+# TODO check return type (any vs UNIT_T)
+def load_parsers() -> Mapping[str, UnitParser[Any]]:
+    lifting_parsers = {u.name: LiftingParser() for u in LiftingUnitName}
+    return MappingProxyType(lifting_parsers)
 
 
-def load_mappers() -> dict:
-    lifting_mappers = {u.name: LiftingMapper for u in LiftingUnitName}
-    return lifting_mappers
+def load_mappers() -> Mapping[str, UnitMapper[Any, Any, Any]]:
+    lifting_mappers = {u.name: LiftingMapper() for u in LiftingUnitName}
+    return MappingProxyType(lifting_mappers)
