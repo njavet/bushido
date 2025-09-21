673a65417eacf1584a35526eb0affd0c54cc46ee
Noe Javet <np.javet@gmail.com>
2025-09-21 12:00:09 +0200

    update tests

--- tests/service/test_preprocess_string.py
+++ tests/service/test_preprocess_string.py
@@ -1,60 +1,69 @@
-import unittest
+import pytest
 
 # project imports
-from bushido.utils.parsing import preprocess_input
+from bushido.domain.unit import UnitSpec
+from bushido.service.log_unit import LogUnitService
 
 
-class TestPreprocess(unittest.TestCase):
-    def test_empty_input_string(self):
-        input_string = ''
-        with self.assertRaises(ValueError):
-            preprocess_input(input_string)
-
-    def test_comment_symbol_only(self):
-        input_string = '#'
-        with self.assertRaises(ValueError):
-            preprocess_input(input_string)
-
-    def test_empty_payload(self):
-        input_string = '# this is a comment'
-        with self.assertRaises(ValueError):
-            preprocess_input(input_string)
-
-    def test_correct_input_without_emoji_no_comment(self):
-        input_string = '<emoji> some 101 char 5'
-        unit_name, words, comment = preprocess_input(input_string)
-        self.assertEqual(unit_name, '<emoji>')
-        self.assertEqual(words, ['some', '101', 'char', '5'])
-        self.assertIsNone(comment)
-
-    def test_correct_input_without_emoji_no_comment_but_symbol(self):
-        input_string = '<emoji> 101 5 #'
-        unit_name, words, comment = preprocess_input(input_string)
-        self.assertEqual(unit_name, '<emoji>')
-        self.assertEqual(words, ['101', '5'])
-        self.assertIsNone(comment)
-
-    def test_correct_input_without_emoji_full(self):
-        input_string = '<emoji> some numbers eg 5 # this is a comment'
-        unit_name, words, comment = preprocess_input(input_string)
-        self.assertEqual(unit_name, '<emoji>')
-        self.assertEqual(words, ['some', 'numbers', 'eg', '5'])
-        self.assertEqual(comment, 'this is a comment')
-
-    def test_correct_input_without_emoji_no_payload(self):
-        input_string = '<emoji> # this is a comment'
-        unit_name, words, comment = preprocess_input(input_string)
-        self.assertEqual(unit_name, '<emoji>')
-        self.assertEqual(words, [])
-        self.assertEqual(comment, 'this is a comment')
-
-    def test_correct_input_without_emoji_no_payload_no_comment(self):
-        input_string = '<emoji>'
-        unit_name, words, comment = preprocess_input(input_string)
-        self.assertEqual(unit_name, '<emoji>')
-        self.assertEqual(words, [])
-        self.assertIsNone(comment)
+@pytest.fixture
+def service():
+    return LogUnitService()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_empty_input_string(service):
+    input_string = ''
+    with pytest.raises(ValueError):
+        service.preprocess_input(input_string)
+
+
+def test_comment_symbol_only(service):
+    input_string = '#'
+    with pytest.raises(ValueError):
+        service.preprocess_input(input_string)
+
+
+def test_empty_payload(service):
+    input_string = '# this is a comment'
+    with pytest.raises(ValueError):
+        service.preprocess_input(input_string)
+
+
+def test_correct_input_syntax_without_comment(service):
+    input_string = 'husserl some 101 char 5'
+    unit_spec = service.preprocess_input(input_string)
+    assert unit_spec.unit_name == 'husserl'
+    assert unit_spec.words == ['some', '101', 'char', '5']
+    assert unit_spec.comment is None
+
+
+def test_correct_input_syntax_without_comment_but_symbol(service):
+    input_string = 'platon 101 5 #'
+    unit_spec = service.preprocess_input(input_string)
+    assert unit_spec.unit_name == 'platon'
+    assert unit_spec.words == ['101', '5']
+    assert unit_spec.comment is None
+
+
+def test_correct_input_syntax_full(service):
+    input_string = 'schopenhauer some numbers eg 5 # this is a comment'
+    unit_spec = service.preprocess_input(input_string)
+    assert unit_spec.unit_name == 'schopenhauer'
+    assert unit_spec.words == ['some', 'numbers', 'eg', '5']
+    assert unit_spec.comment == 'this is a comment'
+
+
+def test_correct_input_syntax_no_payload(service):
+    input_string = 'kant # this is a comment'
+    unit_spec = service.preprocess_input(input_string)
+    assert unit_spec.unit_name == 'kant'
+    assert unit_spec.words == []
+    assert unit_spec.comment == 'this is a comment'
+
+
+def test_correct_input_syntax_no_payload_no_comment(service):
+    input_string = 'wittgenstein'
+    unit_spec = service.preprocess_input(input_string)
+    assert unit_spec.unit_name == 'wittgenstein'
+    assert unit_spec.words == []
+    assert unit_spec.comment is None
+
