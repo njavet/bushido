0ecc019673f47e4c97a0c29b7dacde9723f74d18
Noe Javet <np.javet@gmail.com>
2025-09-21 11:45:47 +0200

    logger factory

---
+++ bushido/core/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+
+class LoggerFactory:
+    @staticmethod
+    def create_logger(obj):
+        name = type(obj).__name__
+        return LoggerFactory.create_static_class_logger(name)
+
+    @staticmethod
+    def create_static_class_logger(class_name: str):
+        logger = logging.getLogger(class_name)
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        return logger
