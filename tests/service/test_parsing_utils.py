45a23d8562e54e90f4bfb224b208880100364fdf
Noe Javet <np.javet@gmail.com>
2025-09-21 16:22:03 +0200

    fix tests

--- tests/service/test_parsing_utils.py
+++ tests/service/test_parsing_utils.py
@@ -1,65 +1,75 @@
 import datetime
 
 # project imports
+from bushido.core.result import Ok
 from bushido.service.parser.utils import (
-    find_previous_sunday,
+    time_string_to_seconds,
     parse_military_time_string,
-    parse_time_string,
+    find_previous_sunday,
 )
 
 
-def test_parse_time_string_HH_MM_SS_0():
+def test_time_string_to_seconds_HH_MM_SS_0():
     ts = '02:24:59'
-    sec = parse_time_string(ts)
-    assert 8699.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 8699.0 == result.value
 
 
-def test_parse_time_string_HH_MM_SS_1():
+def test_time_string_to_seconds_HH_MM_SS_1():
     ts = '1:02:03'
-    sec = parse_time_string(ts)
-    assert 3723.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 3723.0 == result.value
 
 
-def test_parse_time_string_HH_MM_SS_2():
+def test_time_string_to_seconds_HH_MM_SS_2():
     ts = '2:4:1'
-    sec = parse_time_string(ts)
-    assert 7441.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 7441.0 == result.value
 
 
-def test_parse_time_string_MM_SS_0():
+def test_time_string_to_seconds_MM_SS_0():
     ts = '32:29'
-    sec = parse_time_string(ts)
-    assert 1949.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 1949.0 == result.value
 
 
-def test_parse_time_string_MM_SS_2():
+def test_time_string_to_seconds_MM_SS_2():
     ts = '1:20'
-    sec = parse_time_string(ts)
-    assert 80.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 80.0 == result.value
 
 
-def test_parse_time_string_minutes():
+def test_time_string_to_seconds_minutes():
     ts = '16'
-    sec = parse_time_string(ts)
-    assert 960.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 960.0 == result.value
 
 
-def test_parse_time_string_hours():
+def test_time_string_to_seconds_hours():
     ts = '2h'
-    sec = parse_time_string(ts)
-    assert 7200.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 7200.0 == result.value
 
 
-def test_parse_time_string_seconds():
+def test_time_string_to_seconds_seconds():
     ts = '32s'
-    sec = parse_time_string(ts)
-    assert 32.0 == sec
+    result = time_string_to_seconds(ts)
+    assert isinstance(result, Ok)
+    assert 32.0 == result.value
 
 
 def test_parse_military_time_string_0():
     ts = '1600'
-    mt = parse_military_time_string(ts)
-    assert datetime.time(16, 0) == mt
+    result = parse_military_time_string(ts)
+    assert isinstance(result, Ok)
+    assert datetime.time(16, 0) == result.value
 
 
 def test_find_previous_sunday():
