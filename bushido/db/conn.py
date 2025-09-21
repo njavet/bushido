620c66ed85423d876466e310ea58bdb4183fde5a
Noe Javet <np.javet@gmail.com>
2025-09-21 14:27:46 +0200

    read only sessionfactory attrs

--- bushido/db/conn.py
+++ bushido/db/conn.py
@@ -5,17 +5,33 @@ from sqlalchemy.orm import sessionmaker
 
 # project imports
 from bushido.core.conf import DB_URL
+from bushido.db.model.base import Base
 
 
 class SessionFactory:
     def __init__(self, db_url=DB_URL):
-        self._fronzen = False
+        self._frozen = False
         self.db_url = db_url
         self._engine = create_engine(db_url)
         self._sessionmaker = sessionmaker(
             bind=self._engine, expire_on_commit=False
         )
-        self._fronzen = True
+        self._frozen = True
+
+    @property
+    def engine(self):
+        return self._engine
+
+    @property
+    def db_url(self):
+        return self._db_url
+
+    @db_url.setter
+    def db_url(self, db_url):
+        if getattr(self, '_frozen', True):
+            raise AttributeError('db_url is read only after init...')
+        else:
+            self._db_url = db_url
 
     def get_session(self):
         db = self._sessionmaker()
@@ -31,3 +47,9 @@ class SessionFactory:
             yield db
         finally:
             db.close()
+
+    def _setup_engine(self):
+        engine = create_engine(self.db_url)
+        Base.metadata.create_all(bind=engine)
+        return engine
+
