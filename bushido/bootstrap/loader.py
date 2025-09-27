5c9a8e05738b0cf891e02275a6b83f11e00bca06
Noe Javet <np.javet@gmail.com>
2025-09-27 18:24:40 +0200

    run ruff format and check fix

--- bushido/bootstrap/loader.py
+++ bushido/bootstrap/loader.py
@@ -2,8 +2,8 @@ from types import MappingProxyType
 from typing import Any, Mapping
 
 from bushido.core.unit import LiftingUnitName
-from bushido.iface.mapper.base import UnitMapper
 from bushido.iface.mapper import LiftingMapper
+from bushido.iface.mapper.base import UnitMapper
 from bushido.iface.parser.base import UnitParser
 from bushido.iface.parser.lifting import LiftingParser
 
