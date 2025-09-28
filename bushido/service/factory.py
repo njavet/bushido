9fc93e8bd87fcbe3992ebbcdd87b779d36bd1145
Noe Javet <np.javet@gmail.com>
2025-09-28 20:29:47 +0200

    service factory

--- bushido/service/factory.py
+++ bushido/service/factory.py
@@ -1,17 +1,24 @@
 from sqlalchemy.orm import Session
 
-from bushido.core.unit import LiftingUnitName
-from bushido.infra.repo.unit import UnitRepo
-from bushido.infra.db import LiftingUnit, LiftingSet
-from bushido.iface.parser.lifting import LiftingParser
+from bushido.core.conf import UnitCategory
+from bushido.core.result import Err, Ok, Result
 from bushido.iface.mapper.lifting import LiftingMapper
+from bushido.iface.parser.lifting import LiftingParser
+from bushido.infra.db import LiftingSet, LiftingUnit
+from bushido.infra.repo.unit import UnitRepo
 from bushido.service.base import LogUnitService
 
 
 class ServiceFactory:
-    def get_service(self, unit_name: str, session: Session) -> LogUnitService:
-        if unit_name in [u.name for u in LiftingUnitName]:
-            parser = LiftingParser()
-            mapper = LiftingMapper()
-            repo = UnitRepo[LiftingUnit, LiftingSet](session)
-            return LogUnitService(parser, mapper, repo)
+    def get_service(
+        self, unit_category: UnitCategory, session: Session
+    ) -> Result[LogUnitService]:
+        match unit_category:
+            case UnitCategory.LIFTING:
+                parser = LiftingParser()
+                mapper = LiftingMapper()
+                repo = UnitRepo[LiftingUnit, LiftingSet](session)
+                # TODO mypy is fine, pycharm not
+                return Ok(LogUnitService(parser, mapper, repo))
+            case _:
+                return Err('no such unit category')
