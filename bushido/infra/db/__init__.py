f421a9e40d86005818a81d79d0e644888fceea5e
Noe Javet <np.javet@gmail.com>
2025-09-28 17:08:57 +0200

    orm

--- bushido/infra/db/__init__.py
+++ bushido/infra/db/__init__.py
@@ -1,4 +1,3 @@
-__all__ = ['Unit', 'Subunit', 'LiftingUnit', 'LiftingSet']
+__all__ = ['LiftingUnit', 'LiftingSet']
 
-from .model.base import Unit, Subunit
 from .model.lifting import LiftingSet, LiftingUnit
