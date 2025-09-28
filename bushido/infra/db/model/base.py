69cf679f58ee13465cddec3afeb0b7f67831c824
Noe Javet <np.javet@gmail.com>
2025-09-28 20:57:12 +0200

    utc upload date to unit

--- bushido/infra/db/model/base.py
+++ bushido/infra/db/model/base.py
@@ -1,3 +1,4 @@
+import datetime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -11,3 +12,4 @@ class Unit(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column()
     comment: Mapped[str | None] = mapped_column()
+    upload_t: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.timezone.utc))
