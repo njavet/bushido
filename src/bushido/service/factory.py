d86d6328604fd6b2969618b887e26fdedf13d131
noe <noe.javet@risktec.io>
2025-10-03 16:13:39 +0200

    wimhof service factory

--- src/bushido/service/factory.py
+++ src/bushido/service/factory.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import Session
 
 from bushido.domain.base import Err, Ok, Result, UnitCategory
 from bushido.iface.mapper import GymMapper, LiftingMapper
+from bushido.iface.mapper.wimhof import WimhofMapper
 from bushido.iface.parser import GymParser, LiftingParser
-from bushido.infra.db import GymUnit, LiftingSet, LiftingUnit
+from bushido.iface.parser.wimhof import WimhofParser
+from bushido.infra.db import GymUnit, LiftingSet, LiftingUnit, WimhofUnit
 from bushido.infra.repo.unit import UnitRepo
 from bushido.service.base import LogUnitService
 
@@ -28,5 +30,10 @@ class ServiceFactory:
                 gym_mapper = GymMapper()
                 gym_repo = UnitRepo[GymUnit, Any](session, GymUnit, None)
                 return Ok(LogUnitService(gym_parser, gym_mapper, gym_repo))
+            case UnitCategory.wimhof:
+                wimhof_parser = WimhofParser()
+                wimhof_mapper = WimhofMapper()
+                wimhof_repo = UnitRepo[WimhofUnit, Any](session, WimhofUnit, None)
+                return Ok(LogUnitService(wimhof_parser, wimhof_mapper, wimhof_repo))
             case _:
                 return Err("no such unit category")
