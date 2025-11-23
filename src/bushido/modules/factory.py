0191a8bf3ff962fb2810fe3ee68f7ba1ee5f525a
Noe Javet <np.javet@gmail.com>
2025-11-23 18:46:43 +0100

    factory

--- src/bushido/modules/factory.py
+++ src/bushido/modules/factory.py
@@ -2,49 +2,62 @@ from typing import Any
 
 from sqlalchemy.orm import Session
 
-from bushido.modules.dtypes import UnitData, UnitMapper
+from bushido.modules.dtypes import TS, TU, TUData, UnitData, UnitMapper
+from bushido.modules.gym import GymMapper, GymParser, GymUnit, GymUnitName
+from bushido.modules.lifting import (
+    LiftingMapper,
+    LiftingParser,
+    LiftingSet,
+    LiftingUnit,
+    LiftingUnitName,
+)
 from bushido.modules.parser import UnitParser
+from bushido.modules.repo import UnitRepo
+from bushido.modules.wimhof import (
+    WimhofMapper,
+    WimhofParser,
+    WimhofRound,
+    WimhofUnit,
+    WimhofUnitName,
+)
 
 
 class Factory:
     def __init__(self) -> None:
-        self.parser = self.get_parser()
+        self.parser = self.get_parsers()
         self.mappers = self.get_mappers()
 
-    def get_parsers(self) -> dict[str, UnitParser[UnitData]]:
-        if unit_name in [un.name for un in GymUnitName]:
-            return Ok(GymParser(unit_name=unit_name))
-        elif unit_name in [un.name for un in LiftingUnitName]:
-            return Ok(LiftingParser(unit_name=unit_name))
-        elif unit_name in [un.name for un in WimhofUnitName]:
-            return Ok(WimhofParser(unit_name=unit_name))
-        else:
-            return Err(message="No such unit name")
+    @staticmethod
+    def get_parsers() -> dict[str, UnitParser[UnitData]]:
+        result = {}
+        for unit_name in GymUnitName.__members__:
+            result[unit_name] = GymParser(unit_name=unit_name)
+        for unit_name in LiftingUnitName.__members__:
+            result[unit_name] = LiftingParser(unit_name=unit_name)
+        for unit_name in WimhofUnitName.__members__:
+            result[unit_name] = WimhofParser(unit_name=unit_name)
+        return result
 
-    def get_mapper(unit_name: str) -> Result[UnitMapper]:
-        if unit_name in [un.name for un in GymUnitName]:
-            return Ok(GymMapper())
-        elif unit_name in [un.name for un in LiftingUnitName]:
-            return Ok(LiftingMapper())
-        elif unit_name in [un.name for un in WimhofUnitName]:
-            return Ok(WimhofMapper())
-        else:
-            return Err(message="No such unit name")
+    @staticmethod
+    def get_mappers() -> dict[str, UnitMapper[TUData, TU, TS]]:
+        result = {}
+        for unit_name in GymUnitName.__members__:
+            result[unit_name] = GymMapper()
+        for unit_name in LiftingUnitName.__members__:
+            result[unit_name] = LiftingMapper()
+        for unit_name in WimhofUnitName.__members__:
+            result[unit_name] = WimhofMapper()
+        return result
 
-    def get_repo(unit_name: str, session: Session) -> Result[UnitRepo[UnitData]]:
+    @staticmethod
+    def get_repo(unit_name: str, session: Session) -> UnitRepo[TU, TS] | None:
         if unit_name in [un.name for un in GymUnitName]:
-            return Ok(UnitRepo[GymUnit, Any](session=session, unit_cls=GymUnit))
+            return UnitRepo[GymUnit, Any](session=session, unit_cls=GymUnit)
         elif unit_name in [un.name for un in LiftingUnitName]:
-            return Ok(
-                UnitRepo[LiftingUnit, LiftingSet](
-                    session=session, unit_cls=LiftingUnit, subrels=LiftingUnit.subunits
-                )
+            return UnitRepo[LiftingUnit, LiftingSet](
+                session=session, unit_cls=LiftingUnit, subrels=LiftingUnit.subunits
             )
         elif unit_name in [un.name for un in WimhofUnitName]:
-            return Ok(
-                UnitRepo[WimhofUnit, WimhofRound](
-                    session=session, unit_cls=WimhofUnit, subrels=WimhofUnit.subunits
-                )
+            return UnitRepo[WimhofUnit, WimhofRound](
+                session=session, unit_cls=WimhofUnit, subrels=WimhofUnit.subunits
             )
-        else:
-            return Err(message="No such unit name")
