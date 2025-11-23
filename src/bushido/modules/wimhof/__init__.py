d87bc6d35b23d54d76417d136ceeccf0bc60dd01
Noe Javet <np.javet@gmail.com>
2025-11-23 18:26:35 +0100

    wimhof interface

---
+++ src/bushido/modules/wimhof/__init__.py
@@ -0,0 +1,14 @@
+from .dtypes import RoundSpec, WimhofSpec, WimhofUnitName
+from .mapper import WimhofMapper
+from .orm import WimhofRound, WimhofUnit
+from .parser import WimhofParser
+
+__all__ = [
+    "WimhofUnit",
+    "RoundSpec",
+    "WimhofUnitName",
+    "WimhofRound",
+    "WimhofParser",
+    "WimhofMapper",
+    "WimhofSpec",
+]
