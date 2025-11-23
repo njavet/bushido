773c9503c73b285e88c8242859ec026dc0a46493
Noe Javet <np.javet@gmail.com>
2025-11-23 17:56:30 +0100

    modules exports

---
+++ src/bushido/modules/__init__.py
@@ -0,0 +1,3 @@
+from .dtypes import Err, Ok, ParsedUnit, Result
+
+__all__ = ["ParsedUnit", "Result", "Ok", "Err"]
--- src/bushido/modules/gym/__init__.py
+++ src/bushido/modules/gym/__init__.py
@@ -1,6 +1,6 @@
-from .dtypes import GymSpec
+from .dtypes import GymSpec, GymUnitName
 from .mapper import GymMapper
 from .orm import GymUnit
 from .parser import GymParser
 
-__all__ = ["GymSpec", "GymParser", "GymMapper", "GymUnit"]
+__all__ = ["GymSpec", "GymParser", "GymMapper", "GymUnit", "GymUnitName"]
