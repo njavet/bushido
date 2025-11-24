4a9cb446024708c9b259b454c9bae36a90845d36
Noe Javet <np.javet@gmail.com>
2025-11-24 14:15:42 +0100

    fix unit tests

--- tests/unit/test_lifting.py
+++ tests/unit/test_lifting.py
@@ -1,54 +1,60 @@
 import pytest
 
 from bushido.modules.dtypes import Ok, ParsedUnit
-from bushido.modules.lifting.dtypes import SetSpec
+from bushido.modules.lifting.dtypes import LiftingSpec, SetSpec
 from bushido.modules.lifting.parser import LiftingParser
 
 
 @pytest.fixture
 def parser():
-    return LiftingParser()
+    return LiftingParser(unit_name="squat")
 
 
 @pytest.mark.parametrize(
     "line, expected",
     [
         (
-            "squat 100 5 180 100 5",
+            "100 5 180 100 5",
             ParsedUnit(
                 name="squat",
-                data=[
-                    SetSpec(set_nr=0, weight=100.0, reps=5, rest=180.0),
-                    SetSpec(set_nr=1, weight=100.0, reps=5, rest=0.0),
-                ],
+                data=LiftingSpec(
+                    sets=[
+                        SetSpec(set_nr=0, weight=100.0, reps=5, rest=180.0),
+                        SetSpec(set_nr=1, weight=100.0, reps=5, rest=0.0),
+                    ]
+                ),
                 comment=None,
             ),
         ),
         (
-            "squat 120 5",
+            "120 5",
             ParsedUnit(
                 name="squat",
-                data=[SetSpec(set_nr=0, weight=120.0, reps=5, rest=0.0)],
+                data=LiftingSpec([SetSpec(set_nr=0, weight=120.0, reps=5, rest=0.0)]),
                 comment=None,
             ),
         ),
         (
-            "squat 150 3 300 160 2 90 100 20 # heavy day, 20reps at the end",
+            "150 3 300 160 2 90 100 20 # heavy day, 20reps at the end",
             ParsedUnit(
                 name="squat",
-                data=[
-                    SetSpec(set_nr=0, weight=150.0, reps=3, rest=300.0),
-                    SetSpec(set_nr=1, weight=160.0, reps=2.0, rest=90.0),
-                    SetSpec(set_nr=2, weight=100.0, reps=20.0, rest=0.0),
-                ],
+                data=LiftingSpec(
+                    sets=[
+                        SetSpec(set_nr=0, weight=150.0, reps=3, rest=300.0),
+                        SetSpec(set_nr=1, weight=160.0, reps=2.0, rest=90.0),
+                        SetSpec(set_nr=2, weight=100.0, reps=20.0, rest=0.0),
+                    ]
+                ),
                 comment="heavy day, 20reps at the end",
             ),
         ),
         (
-            "deadlift 150 5 # just a single set",
+            "150 5 # just a single set",
             ParsedUnit(
-                name="deadlift",
-                data=[SetSpec(set_nr=0, weight=150.0, reps=5.0, rest=0.0)],
+                name="squat",
+                data=LiftingSpec(
+                    sets=[SetSpec(set_nr=0, weight=150.0, reps=5.0, rest=0.0)]
+                ),
                 comment="just a single set",
             ),
         ),
