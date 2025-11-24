aeecf9f4542363671d47380e5e875ec1dfd2bd1f
Noe Javet <np.javet@gmail.com>
2025-11-24 17:09:53 +0100

    add emojis

--- src/bushido/tui/tui.py
+++ src/bushido/tui/tui.py
@@ -1,7 +1,6 @@
 from textual import events
 from textual.app import App, ComposeResult
 from textual.containers import Grid
-from textual.screen import ModalScreen
 from textual.suggester import Suggester
 from textual.widgets import Footer, Header, Input, Label, Log, RichLog
 
@@ -29,6 +28,12 @@ class BushidoApp(App[None]):
         self.text_log.write("Bushido TUI ready. Type commands like\n:")
         yield self.text_log
         yield self.input
+        yield Grid(
+            Label("Unit Log"),
+            TextInput(suggester=UnitSuggester(self.um)),
+            RichLog(id="response"),
+            id="unit_log",
+        )
         yield Footer()
 
     def on_mount(self) -> None:
@@ -61,29 +66,14 @@ class BushidoApp(App[None]):
         return res
 
 
-class TxUnitManager(ModalScreen):
-    BINDINGS = [("q", "app.pop_screen", "Back"), ("m", "app.pop_screen", "Back")]
-
-    def __init__(self, um, tg_agent):
-        super().__init__()
-        self.um = um
-        self.tg_agent = tg_agent
-
-    def compose(self) -> ComposeResult:
-        yield Grid(
-            Label("Unit Log"),
-            TextInput(suggester=UnitSuggester(self.um)),
-            RichLog(id="response"),
-            id="unit_log",
-        )
-
+"""
     async def on_input_submitted(self, event: Input.Submitted) -> None:
         rl = self.query_one("#response", RichLog)
         rl.clear()
-        msg = await self.tg_agent.send_message("csm101_bot", event.value)
         rl.write(msg.message)
         self.query_one(Input).action_delete_left_all()
         self.query_one(Input).action_delete_right_all()
+"""
 
 
 class TextInput(Input):
@@ -101,9 +91,9 @@ class TextInput(Input):
 
 
 class UnitSuggester(Suggester):
-    def __init__(self, um):
+    def __init__(self, un2emoji):
         super().__init__()
-        self.um = um
+        self.un2emoji = un2emoji
         self.uname2umoji = self.construct_dict()
 
     def construct_dict(self):
