183f76a10eb2e894f4275bdf70497eabff163c4f
Noe Javet <np.javet@gmail.com>
2025-11-24 15:21:58 +0100

    log unit service

--- src/bushido/service/log_unit.py
+++ src/bushido/service/log_unit.py
@@ -1,19 +1,25 @@
 from sqlalchemy.orm import Session
 
 from bushido.modules.dtypes import Err, Ok, Result
-from bushido.modules.factory import get_mappers, get_parsers, get_repo
+from bushido.modules.factory import Factory
 
 
-def log_unit(line: str, session: Session) -> Result[str]:
-    # TODO redesign / validation
+def log_unit(line: str, factory: Factory, session: Session) -> Result[str]:
     unit_name, payload = line.split(" ", 1)
 
-    parsers = get_parsers()
-    mappers = get_mappers()
-    parser = parsers.get(unit_name)
-    mapper = mappers.get(unit_name)
+    parser_res = factory.get_parser(unit_name)
+    if isinstance(parser_res, Err):
+        return parser_res
+    parser = parser_res.value
+    mapper_res = factory.get_mapper(unit_name)
+    if isinstance(mapper_res, Err):
+        return mapper_res
+    mapper = mapper_res.value
 
-    repo = get_repo(unit_name, session)
+    repo_res = factory.get_repo(unit_name, session)
+    if isinstance(repo_res, Err):
+        return repo_res
+    repo = repo_res.value
     parse_res = parser.parse(payload)
     if isinstance(parse_res, Err):
         return parse_res
