06ce2bce9a6d13ab6a2183385e37cde15203464b
Noe Javet <np.javet@gmail.com>
2025-11-25 18:51:18 +0100

    wip

--- src/bushido/tui/txscreens/mind.py
+++ src/bushido/tui/txscreens/mind.py
@@ -110,36 +110,29 @@ class Lecture(Container):
 
 
 class StopwatchTimeDisplay(Static):
-    """A widget to display elapsed time."""
 
     start_time = reactive(time.monotonic())
     seconds = reactive(0.0)
     total = reactive(0.0)
 
     def on_mount(self) -> None:
-        """Event handler called when widget is added to the app."""
-        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
 
     def update_time(self) -> None:
-        """Method to update time to current."""
         self.seconds = self.total + (time.monotonic() - self.start_time)
 
     def watch_total(self, total):
         print(self.total)
 
     def watch_seconds(self, seconds: float) -> None:
-        """Called when the time attribute changes."""
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
         self.update(f"{h:02,.0f}:{m:02.0f}:{s:05.2f}")
 
     def start(self) -> None:
-        """Method to start (or resume) time updating."""
         self.start_time = time.monotonic()
         self.update_timer.resume()
 
     def pause(self):
-        """Method to stop the time display updating."""
         self.update_timer.pause()
         self.total += time.monotonic() - self.start_time
         self.seconds = self.total
@@ -150,7 +143,6 @@ class StopwatchTimeDisplay(Static):
 
 
 class Stopwatch(Container):
-    """A stopwatch widget."""
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         button_id = event.button.id
