e8975221d62a0bac3fd0debf7c63becc430cecb4
Noe Javet <np.javet@gmail.com>
2025-11-27 14:44:32 +0100

    unit log service

--- src/bushido/service/log_unit.py
+++ src/bushido/service/log_unit.py
@@ -1,40 +1,45 @@
 from sqlalchemy.orm import Session
 
-from bushido.modules.dtypes import Err, Ok, ParsedUnit, Result
+from bushido.core.result import Err, Ok, Result
+from bushido.modules.dtypes import ParsedUnit
 from bushido.modules.factory import Factory
 
 
-def log_unit(line: str, factory: Factory, session: Session) -> Result[ParsedUnit]:
-    try:
-        unit_name, payload = line.split(" ", 1)
-    except ValueError:
-        unit_name, payload = line, ""
+class LogUnit:
+    def __init__(self, factory: Factory):
+        self.factory = factory
 
-    # fetch log classes
-    parser_res = factory.get_parser(unit_name)
-    if isinstance(parser_res, Err):
-        return parser_res
-    parser = parser_res.value
+    def log_unit(self, line: str, session: Session) -> Result[ParsedUnit]:
+        try:
+            unit_name, payload = line.split(" ", 1)
+        except ValueError:
+            unit_name, payload = line, ""
 
-    mapper_res = factory.get_mapper(unit_name)
-    if isinstance(mapper_res, Err):
-        return mapper_res
-    mapper = mapper_res.value
+        # fetch log classes
+        parser_res = self.factory.get_parser(unit_name)
+        if isinstance(parser_res, Err):
+            return parser_res
+        parser = parser_res.value
 
-    repo_res = factory.get_repo(unit_name, session)
-    if isinstance(repo_res, Err):
-        return repo_res
-    repo = repo_res.value
+        mapper_res = self.factory.get_mapper(unit_name)
+        if isinstance(mapper_res, Err):
+            return mapper_res
+        mapper = mapper_res.value
 
-    # parse and store
-    parse_res = parser.parse(payload)
-    if isinstance(parse_res, Err):
-        return parse_res
-    else:
-        parsed_unit = parse_res.value
+        repo_res = self.factory.get_repo(unit_name, session)
+        if isinstance(repo_res, Err):
+            return repo_res
+        repo = repo_res.value
 
-    unit, subunits = mapper.to_orm(parsed_unit)
-    if repo.add_unit(unit, subunits):
-        return Ok(parsed_unit)
-    else:
-        return Err("error")
+        # parse and store
+        parse_res = parser.parse(payload)
+        if isinstance(parse_res, Err):
+            return parse_res
+        else:
+            parsed_unit = parse_res.value
+
+        unit, subunits = mapper.to_orm(parsed_unit)
+        if repo.add_unit(unit, subunits):
+            return Ok(parsed_unit)
+        else:
+            return Err("error")
