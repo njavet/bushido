97eaf64b60996502d03953b326781446c16a0701
Noe Javet <np.javet@gmail.com>
2025-11-27 15:17:46 +0100

    log widgets

--- src/bushido/tui/screens/log_unit.py
+++ src/bushido/tui/screens/log_unit.py
@@ -1,12 +1,13 @@
 from typing import Any
 
-from textual.events import Key
+from textual.app import ComposeResult
+from textual.containers import Grid
 from textual.screen import ModalScreen
-from textual.suggester import Suggester, SuggestionReady
-from textual.widgets import Input
+from textual.widgets import RichLog
 
 from bushido.core.result import Result
 from bushido.modules import ParsedUnit
+from bushido.tui.widgets.log_unit import LogUnitInput, UnitSuggester
 
 
 class LogUnitScreen(ModalScreen[Result[ParsedUnit[Any]]]):
@@ -16,28 +17,8 @@ class LogUnitScreen(ModalScreen[Result[ParsedUnit[Any]]]):
         super().__init__()
         self.unit_names = unit_names
 
-
-class UnitSuggester(Suggester):
-    def __init__(self, unit_names: list[str]) -> None:
-        super().__init__()
-        self.unit_names = unit_names
-
-    async def get_suggestion(self, value: str) -> str | None:
-        names = [name for name in self.unit_names if name.startswith(value)]
-        if len(names) == 1:
-            return names[0] + " "
-        return None
-
-
-class TextInput(Input):
-    def __init__(self, suggester: UnitSuggester) -> None:
-        super().__init__(suggester=suggester, id="text_input")
-
-    def on_suggestion_ready(self, event: SuggestionReady) -> None:
-        self.action_delete_left_all()
-        self.insert_text_at_cursor(event.suggestion)
-
-    def on_key(self, event: Key) -> None:
-        # workaround for accepting autocompletion
-        if event.key == "space":
-            self.action_cursor_right()
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            LogUnitInput(suggester=UnitSuggester(self.unit_names)),
+            RichLog(id="log_result"),
+        )
