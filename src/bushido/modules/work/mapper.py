955dc2c4dcb84f2726e9201a24b4c60991e592d6
Noe Javet <np.javet@gmail.com>
2025-12-31 15:40:34 +0100

    work mapper

--- src/bushido/modules/work/mapper.py
+++ src/bushido/modules/work/mapper.py
@@ -3,37 +3,37 @@ from typing import Any
 from bushido.core.dtypes import ParsedUnit
 from bushido.modules.mapper import UnitMapper
 
-from .domain import GymSpec
-from .orm import GymUnit
+from .domain import WorkSpec
+from .orm import WorkUnit
 
 
-class GymMapper(UnitMapper[GymSpec, GymUnit, Any]):
+class WorkMapper(UnitMapper[WorkSpec, WorkUnit, Any]):
     @staticmethod
-    def to_orm(parsed_unit: ParsedUnit[GymSpec]) -> tuple[GymUnit, list[Any]]:
-        unit = GymUnit(
+    def to_orm(parsed_unit: ParsedUnit[WorkSpec]) -> tuple[WorkUnit, list[Any]]:
+        unit = WorkUnit(
             name=parsed_unit.name,
             log_time=parsed_unit.log_time,
             start_t=parsed_unit.data.start_t,
             end_t=parsed_unit.data.end_t,
             location=parsed_unit.data.location,
-            training=parsed_unit.data.training,
-            focus=parsed_unit.data.focus,
+            employer=parsed_unit.data.employer,
+            project=parsed_unit.data.project,
             comment=parsed_unit.comment,
         )
         return unit, []
 
     # TODO fix Any / None / []
     @staticmethod
-    def from_orm(orms: tuple[GymUnit, list[Any]]) -> ParsedUnit[GymSpec]:
+    def from_orm(orms: tuple[WorkUnit, list[Any]]) -> ParsedUnit[WorkSpec]:
         unit, _ = orms
         pu = ParsedUnit(
             name=unit.name,
-            data=GymSpec(
+            data=WorkSpec(
                 start_t=unit.start_t,
                 end_t=unit.end_t,
                 location=unit.location,
-                training=unit.training,
-                focus=unit.focus,
+                employer=unit.employer,
+                project=unit.project,
             ),
             log_time=unit.log_time,
             comment=unit.comment,
