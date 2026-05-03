894537b2610e7de1ca3ffacdc480ca4e9c3536a6
Noe Javet <np.javet@gmail.com>
2026-05-03 19:03:07 +0200

    grammar

--- src/bushido/units/gym/__init__.py
+++ src/bushido/units/gym/__init__.py
@@ -1,10 +1,9 @@
-from .domain import GYM_GRAMMAR, GymSpec, GymUnitName, format_gym_unit
+from .domain import GymSpec, GymUnitName, format_gym_unit
 from .mapper import GymMapper
 from .orm import GymUnit
 from .parser import GymParser
 
 __all__ = [
-    "GYM_GRAMMAR",
     "GymSpec",
     "GymParser",
     "GymMapper",
