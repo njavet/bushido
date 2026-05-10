745b02bb5ab5c3a0f4f5d3647597cb7c4128094c
Noe Javet <np.javet@gmail.com>
2026-05-10 18:45:49 +0200

    gym repo

--- src/bushido/categories/cardio/repo.py
+++ src/bushido/categories/cardio/repo.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Session
 
 from ..repo import UnitRepo
-from .orm import GymUnitTable
+from .orm import CardioUnitTable
 
 
-class GymRepo(UnitRepo[GymUnitTable]):
+class GymRepo(UnitRepo[CardioUnitTable]):
     def __init__(
         self,
         session: Session,
     ) -> None:
-        super().__init__(session, GymUnitTable)
+        super().__init__(session, CardioUnitTable)
