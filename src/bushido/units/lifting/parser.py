1889e89d1dfde26ab53d42709a62f839def9dde0
Noe Javet <np.javet@gmail.com>
2026-05-16 18:34:26 +0200

    lifting parser

--- src/bushido/units/lifting/parser.py
+++ src/bushido/units/lifting/parser.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 
-from bushido.core.exceptions import ParsingError
-from bushido.core.parsing.dt_parse import parse_start_end_time_string
+from bushido.units.dt_parse import parse_start_end_time_string
+from bushido.units.exceptions import ParsingError
 
 from .unit import LiftingData
 
 
 @dataclass(frozen=True, slots=True)
-class GymParser:
+class LiftingParser:
     @staticmethod
     def parse(tokens: tuple[str, ...]) -> LiftingData:
         start_t, end_t = parse_start_end_time_string(tokens[0])
