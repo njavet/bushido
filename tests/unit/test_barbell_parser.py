2f200beb0daa86766222b8d81b22d40b1d71bac2
Noe Javet <np.javet@gmail.com>
2026-05-16 18:29:57 +0200

    barbell tests

---
+++ tests/unit/test_barbell_parser.py
@@ -0,0 +1,68 @@
+import pytest
+
+from bushido.unit.barbell.parser import BarbellParser
+from bushido.unit.barbell.unit import BarbellData, SetData
+from bushido.unit.exceptions import ParsingError
+
+
+@pytest.fixture
+def parser() -> BarbellParser:
+    return BarbellParser()
+
+
+@pytest.mark.parametrize(
+    "tokens, expected",
+    [
+        (
+            ("100", "5", "180", "100", "5"),
+            BarbellData(
+                sets=[
+                    SetData(set_nr=0, weight=100.0, reps=5, rest=180.0),
+                    SetData(set_nr=1, weight=100.0, reps=5, rest=0.0),
+                ],
+                program=None,
+                variant=None,
+            ),
+        ),
+        (
+            ("120", "5"),
+            BarbellData(
+                sets=[SetData(set_nr=0, weight=120.0, reps=5, rest=0.0)],
+                program=None,
+                variant=None,
+            ),
+        ),
+        (
+            ("150", "3", "300", "160", "2", "90", "100", "20"),
+            BarbellData(
+                sets=[
+                    SetData(set_nr=0, weight=150.0, reps=3, rest=300.0),
+                    SetData(set_nr=1, weight=160.0, reps=2.0, rest=90.0),
+                    SetData(set_nr=2, weight=100.0, reps=20.0, rest=0.0),
+                ],
+                program=None,
+                variant=None,
+            ),
+        ),
+    ],
+)
+def test_correct_lifting_units(
+    parser: BarbellParser, tokens: tuple[str, ...], expected: BarbellData
+) -> None:
+    unit_data = parser.parse(tokens)
+    assert unit_data == expected
+
+
+@pytest.mark.parametrize(
+    "tokens, expected",
+    [
+        (("asdf", "5", "180"), "invalid weight"),
+        (("120", "f9"), "invalid reps"),
+        (("150", "3", "f22"), "invalid rest"),
+    ],
+)
+def test_correct_error_message(
+    parser: BarbellParser, tokens: tuple[str, ...], expected: str
+) -> None:
+    with pytest.raises(ParsingError, match=expected):
+        _ = parser.parse(tokens)
