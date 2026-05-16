4bf239be0dbe6221539b0aa182badb247105aedf
Noe Javet <np.javet@gmail.com>
2026-05-16 18:57:18 +0200

    mypy

--- src/bushido/units/wimhof/mapper.py
+++ src/bushido/units/wimhof/mapper.py
@@ -7,17 +7,17 @@ from .unit import RoundData, WimhofData
 class WimhofMapper:
     @staticmethod
     def to_orm(unit: Unit[WimhofData]) -> WimhofUnitTable:
-        unit = WimhofUnitTable(
+        orm_unit = WimhofUnitTable(
             name=unit.name,
             emoji=unit.emoji,
             log_time=unit.log_time,
             comment=unit.comment,
         )
-        unit.rounds = [
+        orm_unit.rounds = [
             WimhofRound(round_nr=r.round_nr, breaths=r.breaths, retention=r.retention)
             for r in unit.data.rounds
         ]
-        return unit
+        return orm_unit
 
     @staticmethod
     def from_orm(orm_unit: WimhofUnitTable) -> Unit[WimhofData]:
