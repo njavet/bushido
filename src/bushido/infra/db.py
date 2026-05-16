aba6d5c9d8078bdc4fd0a7a0a4e16783bf23e900
Noe Javet <np.javet@gmail.com>
2026-05-16 22:47:22 +0200

    infra

--- src/bushido/infra/db.py
+++ src/bushido/infra/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from bushido.conf import DB_URL
-from bushido.db.db_model import Base
+from bushido.db.model.base import Base
 
 
 class SessionFactory:
