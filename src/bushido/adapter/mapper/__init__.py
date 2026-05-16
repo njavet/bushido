598909497e142899bee6f6753b04600287b04242
Noe Javet <np.javet@gmail.com>
2026-05-16 22:57:44 +0200

    mapper interface

---
+++ src/bushido/adapter/mapper/__init__.py
@@ -0,0 +1,11 @@
+from .cardio import CardioMapper
+from .gym import GymMapper
+from .lifting import LiftingMapper
+from .wimhof import WimhofMapper
+
+__all__ = [
+    "CardioMapper",
+    "GymMapper",
+    "LiftingMapper",
+    "WimhofMapper",
+]
