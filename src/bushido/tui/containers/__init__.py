51bfe43c3beb9ee96492e9bb87d6c2e3f612fc3f
Noe Javet <np.javet@gmail.com>
2026-05-16 18:08:44 +0200

    tui api

--- src/bushido/tui/containers/__init__.py
+++ src/bushido/tui/containers/__init__.py
@@ -1,15 +1,7 @@
-from .cardio import CardioContainer
 from .header import HeaderContainer
 from .lifting import LiftingContainer
-from .training import GymContainer
-from .wimhof import WimhofContainer
-from .work import WorkContainer
 
 __all__ = [
     "HeaderContainer",
-    "GymContainer",
     "LiftingContainer",
-    "CardioContainer",
-    "WorkContainer",
-    "WimhofContainer",
 ]
