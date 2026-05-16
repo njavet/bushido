f4b4e3a356b4e9c895e07e520e173fe2d5e7065e
Noe Javet <np.javet@gmail.com>
2026-05-16 22:53:21 +0200

    api

---
+++ src/bushido/db/model/__init__.py
@@ -0,0 +1,13 @@
+from .cardio import CardioUnitTable
+from .gym import GymUnitTable
+from .lifting import LiftingSet, LiftingUnitTable
+from .wimhof import WimhofRound, WimhofUnitTable
+
+__all__ = [
+    "CardioUnitTable",
+    "GymUnitTable",
+    "LiftingUnitTable",
+    "LiftingSet",
+    "WimhofUnitTable",
+    "WimhofRound",
+]
