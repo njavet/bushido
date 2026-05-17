e74cb5173f6308ef298fc404049ec9e2f4d0e5b2
Noe Javet <np.javet@gmail.com>
2026-05-17 22:34:47 +0200

    load unit service

--- src/bushido/application/services/load_unit_service.py
+++ src/bushido/application/services/load_unit_service.py
@@ -1,9 +1,8 @@
 import datetime
-from typing import Callable
 
 from sqlalchemy.orm import Session
 
-from bushido.domain.dtypes import T_DOMAIN, UnitRegistration, UnitRepo
+from bushido.domain.dtypes import UnitRegistration
 from bushido.domain.units import Unit
 from bushido.domain.units.gym import GymData, gym_unit_settings
 from bushido.domain.units.lifting import LiftingData, lifting_unit_settings
@@ -24,12 +23,8 @@ class LoadUnitService:
     ) -> list[Unit[LiftingData]]:
         # TODO repo per unit type
         unit_name = lifting_unit_settings[0].name
-        return self._load_units(
-            self.registry[unit_name].repo_factory,
-            session,
-            start_t=start_t,
-            end_t=end_t,
-        )
+        repo_factory = self.registry[unit_name].repo_factory
+        return repo_factory(session).fetch_units(start_t=start_t, end_t=end_t)
 
     def load_gym_units(
         self,
@@ -39,18 +34,5 @@ class LoadUnitService:
     ) -> list[Unit[GymData]]:
         # TODO repo per unit type
         unit_name = gym_unit_settings[0].name
-        return self._load_units(
-            self.registry[unit_name].repo_factory,
-            session,
-            start_t=start_t,
-            end_t=end_t,
-        )
-
-    @staticmethod
-    def _load_units(
-        repo_factory: Callable[[Session], UnitRepo[T_DOMAIN]],
-        session: Session,
-        start_t: datetime.datetime | None = None,
-        end_t: datetime.datetime | None = None,
-    ) -> list[Unit[T_DOMAIN]]:
+        repo_factory = self.registry[unit_name].repo_factory
         return repo_factory(session).fetch_units(start_t=start_t, end_t=end_t)
