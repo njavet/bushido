375195672d39293066e2f2acbcf19a7210ae2dd9
Noe Javet <np.javet@gmail.com>
2026-05-17 21:23:04 +0200

    rm tests

--- scripts/load_db.py
+++ scripts/load_db.py
@@ -2,8 +2,8 @@ import json
 import sys
 from typing import Any
 
-from bushido.persistence.sf import SessionFactory
 from bushido.main import init_db
+from bushido.persistence.sf import SessionFactory
 from bushido.registry import build_registry
 from bushido.service import LogUnitService
 
