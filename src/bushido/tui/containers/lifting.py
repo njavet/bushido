a303bf96df4c36a4c12aba716520dcebf3f7a479
Noe Javet <np.javet@gmail.com>
2026-05-17 16:25:59 +0200

    lifting unit names loop

--- src/bushido/tui/containers/lifting.py
+++ src/bushido/tui/containers/lifting.py
@@ -8,44 +8,22 @@ from textual.widgets import (
 )
 
 from bushido.units.base import Unit
-from bushido.units.lifting import LiftingData
+from bushido.units.lifting import LiftingData, lifting_unit_settings
 
 
 class LiftingContainer(Container):
     def compose(self) -> ComposeResult:
         with TabbedContent(id="lifting_tabs"):
-            with TabPane("squat"):
-                yield RichLog(id="squat_stats")
-                yield LiftingTable(id="squat_table")
-            with TabPane("deadlift"):
-                yield RichLog(id="deadlift_stats")
-                yield LiftingTable(id="deadlift_table")
-            with TabPane("benchpress"):
-                yield RichLog(id="benchpress_stats")
-                yield LiftingTable(id="benchpress_table")
-            with TabPane("overheadpress"):
-                yield RichLog(id="overheadpress_stats")
-                yield LiftingTable(id="overheadpress_table")
-            with TabPane("rows"):
-                yield RichLog(id="rows_stats")
-                yield LiftingTable(id="rows_table")
+            for unit_name in lifting_unit_settings:
+                with TabPane(unit_name):
+                    yield RichLog(id=f"{unit_name}_stats")
+                    yield LiftingTable(id=f"{unit_name}_table")
 
     def set_units(self, units: list[Unit[LiftingData]]) -> None:
-        self.query_one("#squat_table", LiftingTable).set_units(
-            [u for u in units if u.name == "squat"]
-        )
-        self.query_one("#deadlift_table", LiftingTable).set_units(
-            [u for u in units if u.name == "deadlift"]
-        )
-        self.query_one("#benchpress_table", LiftingTable).set_units(
-            [u for u in units if u.name == "benchpress"]
-        )
-        self.query_one("#overheadpress_table", LiftingTable).set_units(
-            [u for u in units if u.name == "overheadpress"]
-        )
-        self.query_one("#rows_table", LiftingTable).set_units(
-            [u for u in units if u.name == "rows"]
-        )
+        for unit_name in lifting_unit_settings:
+            self.query_one(f"#{unit_name}_table", LiftingTable).set_units(
+                [u for u in units if u.name == unit_name]
+            )
 
 
 class LiftingTable(DataTable[str]):
