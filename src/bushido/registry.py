955c077b88c01afd159ac9d90b7e70c959b662b3
Noe Javet <np.javet@gmail.com>
2026-05-17 16:18:12 +0200

    registry

--- src/bushido/registry.py
+++ src/bushido/registry.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import selectinload
 
 from bushido.adapter.mapper import CardioMapper, GymMapper, LiftingMapper, WimhofMapper
-from bushido.conf import UnitType
 from bushido.db.model import (
     CardioUnitTable,
     GymUnitTable,
@@ -10,11 +9,10 @@ from bushido.db.model import (
 )
 from bushido.db.repo import UnitRepo
 from bushido.dtypes import UnitRegistration
-from bushido.units.base import UnitSetting
 from bushido.units.cardio import CardioParser, cardio_grammar, cardio_unit_settings
 from bushido.units.gym import GymParser, gym_grammar, gym_unit_settings
 from bushido.units.lifting import LiftingParser, lifting_grammar, lifting_unit_settings
-from bushido.units.wimhof import WimhofParser, wimhof_grammar
+from bushido.units.wimhof import WimhofParser, wimhof_grammar, wimhof_unit_settings
 
 
 def build_registry() -> dict[str, UnitRegistration]:
@@ -39,30 +37,24 @@ def build_registry() -> dict[str, UnitRegistration]:
             grammar=lifting_grammar,
             emoji=unit_setting.emoji,
         )
+    for unit_setting in wimhof_unit_settings:
+        registry[unit_setting.name] = UnitRegistration(
+            parser=WimhofParser(),
+            mapper=WimhofMapper(),
+            repo_factory=lambda session: UnitRepo(
+                session,
+                WimhofUnitTable,
+                load_options=(selectinload(WimhofUnitTable.subunits),),
+            ),
+            grammar=wimhof_grammar,
+            emoji=unit_setting.emoji,
+        )
     for unit_setting in cardio_unit_settings:
-    
-            case UnitType.WIMHOF:
-                registry[unit_setting.name] = UnitRegistration(
-                    parser=WimhofParser(),
-                    mapper=WimhofMapper(),
-                    repo_factory=lambda session: UnitRepo(
-                        session,
-                        WimhofUnitTable,
-                        load_options=(selectinload(WimhofUnitTable.subunits),),
-                    ),
-                    unit_type=UnitType.WIMHOF,
-                    grammar=wimhof_grammar,
-                    emoji=unit_setting.emoji,
-                )
-            case UnitType.CARDIO:
-                registry[unit_setting.name] = UnitRegistration(
-                    parser=CardioParser(),
-                    mapper=CardioMapper(),
-                    repo_factory=lambda session: UnitRepo(session, CardioUnitTable),
-                    unit_type=UnitType.CARDIO,
-                    grammar=cardio_grammar,
-                    emoji=unit_setting.emoji,
-                )
-            case _:
-                raise ValueError(f"unknown unit type {unit_setting.unit_type}")
+        registry[unit_setting.name] = UnitRegistration(
+            parser=CardioParser(),
+            mapper=CardioMapper(),
+            repo_factory=lambda session: UnitRepo(session, CardioUnitTable),
+            grammar=cardio_grammar,
+            emoji=unit_setting.emoji,
+        )
     return registry
--- src/bushido/settings.py
+++ src/bushido/settings.py
@@ -3,6 +3,3 @@ from zoneinfo import ZoneInfo
 TIMEZONE = ZoneInfo("Europe/Zurich")
 
 DAY_START_HOUR = 4
-
-
-UNIT_SETTINGS = []
