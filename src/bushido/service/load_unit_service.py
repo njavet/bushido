84222e16c7805216fc108c8806be0110b218a371
Noe Javet <np.javet@gmail.com>
2026-05-17 16:34:43 +0200

    loader

--- src/bushido/service/load_unit_service.py
+++ src/bushido/service/load_unit_service.py
@@ -1,11 +1,17 @@
 import datetime
+from typing import Callable, TypeVar
 
 from sqlalchemy.orm import Session
 
+from bushido.db.repo import UnitRepo
 from bushido.dtypes import UnitRegistration
+from bushido.protocols import UnitMapper
 from bushido.units import Unit
 from bushido.units.gym import GymData
 from bushido.units.lifting import LiftingData, lifting_unit_settings
+
+T = TypeVar("T")
+TU = TypeVar("TU")
 
 
 class LoadUnitService:
@@ -47,4 +53,16 @@ class LoadUnitService:
         parsed_units = [
             self.registry[unit_name].mapper.from_orm(unit) for unit in units
         ]
+        return parsed_units
+
+    @staticmethod
+    def _load_units(
+        mapper: UnitMapper[T, TU],
+        repo_factory: Callable[[Session], UnitRepo[TU]],
+        session: Session,
+        start_t: datetime.datetime | None = None,
+        end_t: datetime.datetime | None = None,
+    ) -> list[Unit[T]]:
+        units = repo_factory(session).fetch_units(start_t=start_t, end_t=end_t)
+        parsed_units = [mapper.from_orm(unit) for unit in units]
         return parsed_units
