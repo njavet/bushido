c12548705fc4882e852ed9fb885253188f39e1d5
Noe Javet <np.javet@gmail.com>
2026-05-17 21:38:10 +0200

    import

--- src/bushido/persistence/repos/base.py
+++ src/bushido/persistence/repos/base.py
@@ -6,7 +6,8 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.interfaces import ORMOption
 
-from bushido.dtypes import T_DOMAIN, Unit
+from bushido.dtypes import T_DOMAIN
+from bushido.units import Unit
 
 from ..models import UnitTable
 
