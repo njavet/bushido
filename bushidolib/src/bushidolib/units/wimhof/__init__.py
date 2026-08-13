5313e96758671f82010d1159ab59d4517613d386
Noe Javet <np.javet@gmail.com>
2026-08-13 18:43:32 +0200

    parse wimhof unit

--- bushidolib/src/bushidolib/units/wimhof/__init__.py
+++ bushidolib/src/bushidolib/units/wimhof/__init__.py
@@ -1,4 +1,4 @@
-from .parser import Parser as WimhofParser
+from .parser import parse as parse_wimhof_unit
 from .spec import Data as WimhofData
 from .spec import RoundData
 from .spec import grammar as wimhof_grammar
@@ -7,7 +7,7 @@ from .spec import unit_settings as wimhof_unit_settings
 __all__ = [
     "RoundData",
     "WimhofData",
-    "WimhofParser",
+    "parse_wimhof_unit",
     "wimhof_grammar",
     "wimhof_unit_settings",
 ]
--- bushidolib/src/bushidolib/units/wimhof/parser.py
+++ bushidolib/src/bushidolib/units/wimhof/parser.py
@@ -4,24 +4,21 @@ from bushidolib.exceptions import ParsingError
 from .spec import Data, RoundData
 
 
-@dataclass(frozen=True, slots=True)
-class Parser:
-    @staticmethod
-    def parse(tokens: tuple[str, ...]) -> Data:
-        breaths = [int(b) for b in tokens[::2]]
-        retentions = [int(r) for r in tokens[1::2]]
-        if len(breaths) == 0:
-            raise ParsingError("at least one round")
-        if len(breaths) != len(retentions):
-            raise ParsingError(f"breaths and retentions don't match {tokens}")
-        if any(x < 0 for x in breaths):
-            raise ParsingError("breaths must all be positive")
-        if any(x < 0 for x in retentions):
-            raise ParsingError("retentions must all be positive")
+def parse(tokens: tuple[str, ...]) -> Data:
+    breaths = [int(b) for b in tokens[::2]]
+    retentions = [int(r) for r in tokens[1::2]]
+    if len(breaths) == 0:
+        raise ParsingError("at least one round")
+    if len(breaths) != len(retentions):
+        raise ParsingError(f"breaths and retentions don't match {tokens}")
+    if any(x < 0 for x in breaths):
+        raise ParsingError("breaths must all be positive")
+    if any(x < 0 for x in retentions):
+        raise ParsingError("retentions must all be positive")
 
-        return Data(
-            rounds=[
-                RoundData(round_nr=i, breaths=b, retention=r)
-                for i, (b, r) in enumerate(zip(breaths, retentions, strict=False))
-            ]
-        )
+    return Data(
+        rounds=[
+            RoundData(round_nr=i, breaths=b, retention=r)
+            for i, (b, r) in enumerate(zip(breaths, retentions, strict=False))
+        ]
+    )
