da78e7bc9d3af6da42f04285ee3faec0af1d4c3d
Noe Javet <np.javet@gmail.com>
2026-08-15 15:28:01 +0200

    lifting repo

--- bushido-server/src/bushido_server/persistence/repos/_lifting.py
+++ bushido-server/src/bushido_server/persistence/repos/_lifting.py
@@ -1,20 +1,19 @@
 from typing import override
 
-from bushidolib.domain import Unit
 from sqlalchemy.orm import selectinload
 
-from bushidolib.lifting import LiftingData, SetData
+from bushidolib.lifting import LiftingData, LiftingSetData, LiftingUnit
 
 from ..models import LiftingSet, LiftingUnitTable
 from ._base import BaseUnitRepo
 
 
-class LiftingUnitRepo(BaseUnitRepo[LiftingData, LiftingUnitTable]):
+class LiftingUnitRepo(BaseUnitRepo[LiftingUnit, LiftingUnitTable]):
     orm_cls = LiftingUnitTable
     load_options = (selectinload(LiftingUnitTable.subunits),)
 
     @override
-    def _to_orm(self, unit: Unit[LiftingData]) -> LiftingUnitTable:
+    def _to_orm(self, unit: LiftingUnit) -> LiftingUnitTable:
         setting_id = self.get_unit_setting_id(unit.name)
         orm_unit = LiftingUnitTable(
             unit_setting_id=setting_id,
@@ -28,13 +27,13 @@ class LiftingUnitRepo(BaseUnitRepo[LiftingData, LiftingUnitTable]):
         return orm_unit
 
     @override
-    def _from_orm(self, orm_unit: LiftingUnitTable) -> Unit[LiftingData]:
+    def _from_orm(self, orm_unit: LiftingUnitTable) -> LiftingUnit:
         name = self.get_unit_setting_name(orm_unit.unit_setting_id)
         lst = []
         for s in orm_unit.subunits:
-            sp = SetData(set_nr=s.set_nr, weight=s.weight, reps=s.reps, rest=s.rest)
+            sp = LiftingSetData(set_nr=s.set_nr, weight=s.weight, reps=s.reps, rest=s.rest)
             lst.append(sp)
-        return Unit(
+        return LiftingUnit(
             name=name,
             data=LiftingData(sets=lst, program=None, variant=None),
             log_time=orm_unit.log_time,
