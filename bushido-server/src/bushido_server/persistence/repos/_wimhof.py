8e21ad4b18d5398810859b50d42ce7eb32974d4d
Noe Javet <np.javet@gmail.com>
2026-08-15 15:28:48 +0200

    wimhof repo

--- bushido-server/src/bushido_server/persistence/repos/_wimhof.py
+++ bushido-server/src/bushido_server/persistence/repos/_wimhof.py
@@ -1,18 +1,16 @@
 from typing import override
 
-from bushidolib.domain import Unit
-
-from bushidolib.wimhof import RoundData, WimhofData
+from bushidolib.wimhof import WimhofRoundData, WimhofData, WimhofUnit
 
 from ..models import WimhofRound, WimhofUnitTable
 from ._base import BaseUnitRepo
 
 
-class WimhofUnitRepo(BaseUnitRepo[WimhofData, WimhofUnitTable]):
+class WimhofUnitRepo(BaseUnitRepo[WimhofUnit, WimhofUnitTable]):
     orm_cls = WimhofUnitTable
 
     @override
-    def _to_orm(self, unit: Unit[WimhofData]) -> WimhofUnitTable:
+    def _to_orm(self, unit: WimhofUnit) -> WimhofUnitTable:
         setting_id = self.get_unit_setting_id(unit.name)
         orm_unit = WimhofUnitTable(
             unit_setting_id=setting_id,
@@ -26,15 +24,15 @@ class WimhofUnitRepo(BaseUnitRepo[WimhofData, WimhofUnitTable]):
         return orm_unit
 
     @override
-    def _from_orm(self, orm_unit: WimhofUnitTable) -> Unit[WimhofData]:
+    def _from_orm(self, orm_unit: WimhofUnitTable) -> WimhofUnit:
         name = self.get_unit_setting_name(orm_unit.unit_setting_id)
         lst = []
         for r in orm_unit.subunits:
-            ws = RoundData(
+            ws = WimhofRoundData(
                 round_nr=r.round_nr, breaths=r.breaths, retention=r.retention
             )
             lst.append(ws)
-        return Unit(
+        return WimhofUnit(
             name=name,
             data=WimhofData(rounds=lst),
             log_time=orm_unit.log_time,
