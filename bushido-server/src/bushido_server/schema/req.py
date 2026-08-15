9b43c2208b7bea33692bc0101604b7b646ae0e79
Noe Javet <np.javet@gmail.com>
2026-08-15 14:41:48 +0200

    load req

--- bushido-server/src/bushido_server/schema/req.py
+++ bushido-server/src/bushido_server/schema/req.py
@@ -1,5 +1,15 @@
+import datetime
+
 from pydantic import BaseModel, Field
+
+from bushidolib.constants import UnitCategory
 
 
 class LogUnitRequest(BaseModel):
     line: str = Field(min_length=1)
+
+
+class LoadUnitRequest(BaseModel):
+    unit_category: UnitCategory
+    start_time: datetime.datetime | None = None
+    end_time: datetime.datetime | None = None
