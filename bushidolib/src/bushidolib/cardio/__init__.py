c066dfe552664e360cc3ba6ddd7a4378e3a12092
Noe Javet <np.javet@gmail.com>
2026-08-15 15:16:27 +0200

    cardio pkg

--- bushidolib/src/bushidolib/cardio/__init__.py
+++ bushidolib/src/bushidolib/cardio/__init__.py
@@ -1,5 +1,4 @@
 from ._parsing import parse as parse_cardio_unit
 from ._spec import CardioData
-from ._spec import grammar as cardio_grammar
 
-__all__ = ["CardioData", "cardio_grammar", "parse_cardio_unit"]
+__all__ = ["CardioData", "parse_cardio_unit"]
