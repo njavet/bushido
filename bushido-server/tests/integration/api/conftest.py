77f3728c50ee5682fa56ad522b44b5a8eb9c8d8b
Noe Javet <np.javet@gmail.com>
2026-08-15 15:46:15 +0200

    api conftest

---
+++ bushido-server/tests/integration/api/conftest.py
@@ -0,0 +1,32 @@
+from collections.abc import Generator
+from unittest.mock import Mock
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from bushido_server.api import router
+from bushido_server.api.deps import get_session
+
+
+@pytest.fixture
+def session() -> Mock:
+    return Mock(spec=Session)
+
+
+@pytest.fixture
+def app(session: Mock) -> FastAPI:
+    app = FastAPI()
+    app.include_router(router, prefix="/api")
+
+    def override_get_session() -> Generator[Session]:
+        yield session
+
+    app.dependency_overrides[get_session] = override_get_session
+    return app
+
+
+@pytest.fixture
+def client(app: FastAPI) -> TestClient:
+    return TestClient(app)
