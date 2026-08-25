31467a0091995309721cd18196da44799fb048df
njavet <np.javet@gmail.com>
2026-08-25 11:31:57 +0200

    tf vars

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -15,3 +15,30 @@ variable "network_cidr" {
   type        = string
   default     = "10.20.0.0/24"
 }
+
+#-------------------------------------------------------------------------------------
+# azure
+#-------------------------------------------------------------------------------------
+variable "subscription_id" {
+  type = string
+}
+
+variable "unique_suffix" {
+  description = "Makes globally unique Azure resource names"
+  type        = string
+}
+
+variable "container_image" {
+  description = "Docker image accessible by Azure Container Apps"
+  type        = string
+}
+
+variable "sql_admin_user" {
+  type    = string
+  default = "bushidoadmin"
+}
+
+variable "sql_admin_password" {
+  type      = string
+  sensitive = true
+}
