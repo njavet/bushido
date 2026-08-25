fb42a02042ae7497aaa5f53128b1bfbd4b68b61f
njavet <np.javet@gmail.com>
2026-08-25 15:12:20 +0200

    identity

--- infra/azure/terraform/acr.tf
+++ infra/azure/terraform/acr.tf
@@ -1,5 +1,8 @@
-resource "azurerm_role_assignment" "bushido_acr_pull" {
-  scope = azurerm_container_registry.bushido.id
-  role_definition_name = "AcrPull"
-  principal_id = azurerm_container_app.bushido.identity[0].principal_id
+resource "azurerm_container_registry" "bushido" {
+  name = "acrbushido"
+  resource_group_name = azurerm_resource_group.bushido.name
+  location = azurerm_resource_group.bushido.location
+
+  sku = "Basic"
+  admin_enabled = false
 }
