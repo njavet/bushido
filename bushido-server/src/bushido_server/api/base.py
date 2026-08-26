1d670acb27ffdd0cff36cdeb2366b38c6f272e8f
Noe Javet <np.javet@gmail.com>
2026-08-26 14:11:27 +0200

    ruff

--- bushido-server/src/bushido_server/api/base.py
+++ bushido-server/src/bushido_server/api/base.py
@@ -6,16 +6,16 @@ router = APIRouter()
 @router.get("/health/startup")
 def startup() -> dict[str, str]:
     # has initialization completed ?
-    return {'status': 'ok'}
+    return {"status": "ok"}
 
 
 @router.get("/health/live")
 def liveness() -> dict[str, str]:
     # is this process broken ?
-    return {'status': 'ok'}
+    return {"status": "ok"}
 
 
 @router.get("/health/ready")
 def readiness() -> dict[str, str]:
     # should requests send now ?
-    return {'status': 'ok'}
+    return {"status": "ok"}
