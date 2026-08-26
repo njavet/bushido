fad95f76aaabfefcc832a18000519395558e9702
Noe Javet <np.javet@gmail.com>
2026-08-26 14:18:57 +0200

    random suffix

--- infra/azure/terraform/acr.tf
+++ infra/azure/terraform/acr.tf
@@ -1,8 +1,13 @@
-resource "azurerm_container_registry" "bushido" {
-  name = "acrbushido"
-  resource_group_name = azurerm_resource_group.bushido.name
-  location = azurerm_resource_group.bushido.location
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
 
-  sku = "Basic"
-  admin_enabled = false
+resource "azurerm_container_registry" "bushido" {
+  name                = "acrbushido${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.bushido.name
+  location            = azurerm_resource_group.bushido.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
