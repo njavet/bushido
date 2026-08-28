fadc579eac5f4a87a55c54957c1344b044a48cc7
njavet <np.javet@gmail.com>
2026-08-28 13:50:56 +0200

    network

---
+++ infra/azure/terraform/network.tf
@@ -0,0 +1,40 @@
+resource "azurerm_virtual_network" "bushido" {
+  name                = "vnet-${local.prefix}"
+  location            = azurerm_resource_group.bushido.location
+  resource_group_name = azurerm_resource_group.bushido.name
+
+  address_space = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "postgres" {
+  name                 = "snet-postgres"
+  resource_group_name  = azurerm_resource_group.bushido.name
+  virtual_network_name = azurerm_virtual_network.bushido.name
+
+  address_prefixes = ["10.0.1.0/24"]
+
+  delegation {
+    name = "postgres"
+
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "postgres" {
+  name                = "bushido.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.bushido.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
+  name                  = "bushido-postgres"
+  resource_group_name   = azurerm_resource_group.bushido.name
+  private_dns_zone_name = azurerm_private_dns_zone.postgres.name
+  virtual_network_id    = azurerm_virtual_network.bushido.id
+}
--- infra/azure/terraform/postgres.tf
+++ infra/azure/terraform/postgres.tf
@@ -3,17 +3,24 @@ resource "azurerm_postgresql_flexible_server" "bushido" {
   resource_group_name = azurerm_resource_group.bushido.name
   location            = azurerm_resource_group.bushido.location
 
-  version = "16"
+  version = "17"
 
   administrator_login    = var.postgres_admin_user
   administrator_password = var.postgres_admin_password
+
+  delegated_subnet_id    = azurerm_subnet.postgres.id
+  private_dns_zone_id    = azurerm_private_dns_zone.postgres.id
 
   sku_name   = "B_Standard_B1ms"
   storage_mb = 32768
 
   backup_retention_days         = 7
   geo_redundant_backup_enabled  = false
-  public_network_access_enabled = true
+  public_network_access_enabled = false
+
+  depends_on = [
+    azurerm_private_dns_zone_virtual_network_link.postgres,
+  ]
 }
 
 resource "azurerm_postgresql_flexible_server_database" "bushido" {
@@ -22,6 +29,10 @@ resource "azurerm_postgresql_flexible_server_database" "bushido" {
 
   charset   = "UTF8"
   collation = "en_US.utf8"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "azure" {
