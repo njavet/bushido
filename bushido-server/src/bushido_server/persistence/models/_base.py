70d2bc6755ef6048a71d3c7873320009beb7d417
njavet <np.javet@gmail.com>
2026-09-09 15:43:35 +0200

    spartan

--- bushido-server/src/bushido_server/persistence/models/_base.py
+++ bushido-server/src/bushido_server/persistence/models/_base.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -14,6 +14,13 @@ class Spartan(Base):
     __tablename__ = "spartan"
 
     name: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_admin: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.datetime.now(datetime.UTC)
+    )
 
 
 class UnitCategoryTable(Base):
