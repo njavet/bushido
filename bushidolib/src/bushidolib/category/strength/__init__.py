a0e70f9e50d5df24e0a26f4acdb5f7b911cb261c
Noe Javet <np.javet@gmail.com>
2026-09-22 12:21:31 +0200

    strength api

--- bushidolib/src/bushidolib/category/strength/__init__.py
+++ bushidolib/src/bushidolib/category/strength/__init__.py
@@ -1,9 +1,11 @@
-from ._parsing import build_lifting_unit
-from ._spec import LiftingData, LiftingSetData, LiftingUnit
+from ._parsing import build_lifting_unit, build_strength_unit
+from ._spec import LiftingData, LiftingSetData, LiftingUnit, StrengthUnit
 
 __all__ = [
     "LiftingData",
     "LiftingSetData",
     "LiftingUnit",
+    "StrengthUnit",
     "build_lifting_unit",
+    "build_strength_unit",
 ]
