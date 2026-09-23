4021be06bdd069d55522ff893cbcfd444b3a2c17
njavet <np.javet@gmail.com>
2026-09-23 16:11:26 +0200

    ruff format

--- bushido-server/src/bushido_server/persistence/models/_martial_arts.py
+++ bushido-server/src/bushido_server/persistence/models/_martial_arts.py
@@ -8,7 +8,7 @@ from ._base import UnitTable
 class MartialArtsUnitTable(UnitTable):
     __tablename__ = "martial_arts_unit"
 
-    kind: Mapped[str]  = mapped_column()
+    kind: Mapped[str] = mapped_column()
     start_t: Mapped[datetime.time] = mapped_column()
     end_t: Mapped[datetime.time] = mapped_column()
     gym: Mapped[str] = mapped_column()
