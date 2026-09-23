df4969395918805275b2978b6d6c2158c6b0b1eb
njavet <np.javet@gmail.com>
2026-09-23 16:47:59 +0200

    unit registry

--- bushido-server/src/bushido_server/registry.py
+++ bushido-server/src/bushido_server/registry.py
@@ -3,21 +3,31 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any
 
-from bushidolib.category.wimhof import build_wimhof_unit
+from bushidolib.unit.base import BaseUnit, RawUnit
+from bushidolib.unit.cali import build_cali_unit
+from bushidolib.unit.chrono import build_chrono_unit
+from bushidolib.unit.log import build_log_unit
+from bushidolib.unit.wimhof import build_wimhof_unit
+from bushidolib.unit.cardio import build_cardio_unit
+from bushidolib.unit.work import build_work_unit
+from bushidolib.unit.martial_arts import build_martial_arts_unit
+from bushidolib.unit.strength import (
+    build_lifting_unit,
+build_strength_unit,
+)
 
 from bushido_server.persistence.repos import (
     BaseUnitRepo,
-    CardioUnitRepo,
-    GymUnitRepo,
-    LiftingUnitRepo,
+    ChronoUnitRepo,
+    CaliUnitRepo,
+    LogUnitRepo,
+    MartialArtsUnitRepo, WorkUnitRepo,
     WimhofUnitRepo,
-)
-from bushidolib.constants import UnitCategory
-from bushidolib.unit.base import BaseUnit, RawUnit
-from bushidolib.unit.cardio import build_cardio_unit
-from bushidolib.unit.martial_arts import build_gym_unit
-from bushidolib.unit.strength import (
-    build_lifting_unit,
+    StrengthUnitRepo,
+    SwimmingUnitRepo,
+    RunningUnitRepo,
+    RopeSkipUnitRepo,
+    LiftingUnitRepo,
 )
 
 
@@ -27,15 +37,24 @@ class UnitSpec:
     build_unit: Callable[[RawUnit, datetime.datetime], BaseUnit]
 
 
-CATEGORY_REGISTRY: dict[UnitCategory, UnitSpec] = {
-    UnitCategory.CARDIO: UnitSpec(
-        unit_repo=CardioUnitRepo, build_unit=build_cardio_unit
-    ),
-    UnitCategory.GYM: UnitSpec(unit_repo=GymUnitRepo, build_unit=build_gym_unit),
-    UnitCategory.LIFTING: UnitSpec(
-        unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit
-    ),
-    UnitCategory.WIMHOF: UnitSpec(
-        unit_repo=WimhofUnitRepo, build_unit=build_wimhof_unit
-    ),
+UNIT_REGISTRY: dict[str, UnitSpec] = {
+    "cali": UnitSpec(unit_repo=CaliUnitRepo, build_unit=build_cali_unit),
+    "running": UnitSpec(unit_repo=RunningUnitRepo, build_unit=build_cardio_unit),
+    "swimming": UnitSpec(unit_repo=SwimmingUnitRepo, build_unit=build_cardio_unit),
+    "skipping": UnitSpec(unit_repo=RopeSkipUnitRepo, build_unit=build_cardio_unit),
+    "log": UnitSpec(unit_repo=LogUnitRepo, build_unit=build_log_unit),
+    "chrono": UnitSpec(unit_repo=ChronoUnitRepo, build_unit=build_chrono_unit),
+    "karate": UnitSpec(unit_repo=MartialArtsUnitRepo, build_unit=build_martial_arts_unit),
+    "grappling": UnitSpec(unit_repo=MartialArtsUnitRepo, build_unit=build_martial_arts_unit),
+    "strength": UnitSpec(unit_repo=StrengthUnitRepo, build_unit=build_strength_unit),
+    "squat": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "deadlift": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "benchpress": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "overheadpress": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "rows": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "curls": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "shoulder": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "neck": UnitSpec(unit_repo=LiftingUnitRepo, build_unit=build_lifting_unit),
+    "work": UnitSpec(unit_repo=WorkUnitRepo, build_unit=build_work_unit),
+    "wimhof": UnitSpec(unit_repo=WimhofUnitRepo, build_unit=build_wimhof_unit),
 }
---
+++ bushido-server/src/bushido_server/settings.py
@@ -0,0 +1,11 @@
+from bushidolib.constants import UnitCategory
+
+UNIT_NAME_REGISTRY = {
+    "strength": UnitCategory.GYM,
+    "squat": UnitCategory.LIFTING,
+    "deadlift": UnitCategory.LIFTING,
+    "benchpress": UnitCategory.LIFTING,
+    "overheadpress": UnitCategory.LIFTING,
+    "rows": UnitCategory.LIFTING,
+    "wimhof": UnitCategory.WIMHOF,
+}
