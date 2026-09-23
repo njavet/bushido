304a727919fde8c7302d9801c36c3ad525327bff
njavet <np.javet@gmail.com>
2026-09-23 16:38:34 +0200

    settngs

--- bushido-server/src/bushido_server/schema/res.py
+++ bushido-server/src/bushido_server/schema/res.py
@@ -1,8 +1,6 @@
-from bushidolib.category.wimhof import WimhofUnit
 from pydantic import BaseModel
 
 from bushidolib.unit.cardio import CardioUnit
-from bushidolib.unit.martial_arts import GymUnit
 from bushidolib.unit.strength import LiftingUnit
 
 
@@ -10,4 +8,6 @@ class UnitLogResponse(BaseModel):
     status: str
 
 
-LoadedUnits = list[CardioUnit] | list[GymUnit] | list[LiftingUnit] | list[WimhofUnit]
+LoadedUnits = (
+        list[CardioUnit] | list[LiftingUnit]
+)
