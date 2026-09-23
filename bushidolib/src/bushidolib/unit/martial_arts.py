1d54ddfd45f5ab61542ef4b3ef1dc715cbba13ce
njavet <np.javet@gmail.com>
2026-09-23 12:41:04 +0200

    ma

--- bushidolib/src/bushidolib/unit/martial_arts.py
+++ bushidolib/src/bushidolib/unit/martial_arts.py
@@ -6,12 +6,12 @@ from bushidolib.unit.base import BaseUnit, RawUnit, SpaceTimeData
 
 class MartialArtsUnit(BaseUnit, SpaceTimeData):
     kind: str
-    coach: str
 
 
 def build_martial_arts_unit(raw_unit: RawUnit, log_time: datetime.datetime) -> MartialArtsUnit:
     data = parse_space_time_data(raw_unit.tokens)
     return MartialArtsUnit(
+        kind=raw_unit.name,
         log_time=log_time,
         comment=raw_unit.comment,
         start_t=data.start_t,
