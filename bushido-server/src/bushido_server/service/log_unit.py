8461fd88e39871f53711056a6bc604104acd681d
njavet <np.javet@gmail.com>
2026-09-23 16:49:37 +0200

    settings

--- bushido-server/src/bushido_server/service/log_unit.py
+++ bushido-server/src/bushido_server/service/log_unit.py
@@ -3,8 +3,7 @@ import datetime
 from sqlalchemy.orm import Session
 
 from bushido_server.dtypes import Clock, SystemClock
-from bushido_server.registry import CATEGORY_REGISTRY
-from bushido_server.settings import UNIT_NAME_REGISTRY
+from bushido_server.registry import UNIT_REGISTRY
 from bushidolib.exceptions import UnitParsingError
 from bushidolib.unit.base import BaseUnit, RawUnit
 
@@ -14,14 +13,9 @@ def log_unit(line: str, session: Session) -> BaseUnit:
     log_time = resolve_log_time(override=raw_unit.raw_log_time, clock=SystemClock())
 
     try:
-        category = UNIT_NAME_REGISTRY[raw_unit.name]
+        spec = UNIT_REGISTRY[raw_unit.name]
     except KeyError as e:
         raise UnitParsingError(f"Unknown unit: {raw_unit.name}") from e
-
-    try:
-        spec = CATEGORY_REGISTRY[category]
-    except KeyError as e:
-        raise UnitParsingError(f"Unknown unit category: {category}") from e
 
     repo = spec.unit_repo(session)
     unit = spec.build_unit(raw_unit, log_time)
