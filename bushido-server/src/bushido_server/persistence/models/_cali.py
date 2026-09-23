3ded0103373d241fc27d15f74504ca30720b3006
njavet <np.javet@gmail.com>
2026-09-23 16:11:41 +0200

    ruff check fix

--- bushido-server/src/bushido_server/persistence/models/_cali.py
+++ bushido-server/src/bushido_server/persistence/models/_cali.py
@@ -1,4 +1,3 @@
-import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column
 
--- bushido-server/src/bushido_server/persistence/models/_work.py
+++ bushido-server/src/bushido_server/persistence/models/_work.py
@@ -1,4 +1,5 @@
 import datetime
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ._base import UnitTable
