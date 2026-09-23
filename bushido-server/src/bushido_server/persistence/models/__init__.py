e70989ecb4d98503eb763968a96136ebd06924bc
njavet <np.javet@gmail.com>
2026-09-23 16:13:06 +0200

    if

--- bushido-server/src/bushido_server/persistence/models/__init__.py
+++ bushido-server/src/bushido_server/persistence/models/__init__.py
@@ -1,17 +1,24 @@
-from ._base import Base, Spartan, UnitTable
-from ._cardio import CardioUnitTable
-from ._gym import GymUnitTable
-from ._strength import LiftingSet, LiftingUnitTable
+from ._base import Base, Spartan, UnitTable, ChronoUnitTable, LogUnitTable
+from ._cardio import RopeSkipUnitTable, RunningUnitTable, SwimmingUnitTable
+from ._strength import LiftingSet, LiftingUnitTable, StrengthUnitTable
 from ._wimhof import WimhofRound, WimhofUnitTable
+from ._cali import CaliUnitTable
+from ._martial_arts import MartialArtsUnitTable
+from ._work import WorkUnitTable
 
 __all__ = [
     "Base",
-    "CardioUnitTable",
-    "GymUnitTable",
     "LiftingSet",
     "LiftingUnitTable",
     "Spartan",
     "UnitTable",
     "WimhofRound",
     "WimhofUnitTable",
+    "RopeSkipUnitTable",
+    "RunningUnitTable",
+    "SwimmingUnitTable",
+    "StrengthUnitTable",
+    "CaliUnitTable",
+    "MartialArtsUnitTable",
+    "WorkUnitTable",
 ]
--- bushido-server/src/bushido_server/persistence/models/_cali.py
+++ bushido-server/src/bushido_server/persistence/models/_cali.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ._base import UnitTable
